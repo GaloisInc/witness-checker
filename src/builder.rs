e09b9e56f7fa9bdcba4fdb878954b1f690828726
Stuart Pernsteiner <spernsteiner@galois.com>
2020-05-28 09:24:29 -0700

    framework for tinyram trace checking

--- src/builder.rs
+++ src/builder.rs
@@ -4,7 +4,6 @@ use scale_isa::types::{
     Instruction, Imm, RegClearModp, RegClearRegint, RegSecretBit, RegSecretModp, RegSecretRegint,
 };
 
-
 #[derive(Clone, Default)]
 pub struct Counter {
     next: u32,
@@ -31,6 +30,42 @@ pub trait RegType: Copy + Eq {
 
     fn print(self) -> Instruction {
         panic!("print not supported for {}", Self::desc())
+    }
+
+    fn add(self, a: Self, b: Self) -> Instruction {
+        panic!("add not supported for {}", Self::desc())
+    }
+
+    fn sub(self, a: Self, b: Self) -> Instruction {
+        panic!("sub not supported for {}", Self::desc())
+    }
+
+    fn mul(self, a: Self, b: Self) -> Instruction {
+        panic!("mul not supported for {}", Self::desc())
+    }
+
+    fn neg(self, a: Self) -> Instruction {
+        panic!("neg not supported for {}", Self::desc())
+    }
+
+    fn and(self, a: Self, b: Self) -> Instruction {
+        panic!("and not supported for {}", Self::desc())
+    }
+
+    fn or(self, a: Self, b: Self) -> Instruction {
+        panic!("or not supported for {}", Self::desc())
+    }
+
+    fn xor(self, a: Self, b: Self) -> Instruction {
+        panic!("xor not supported for {}", Self::desc())
+    }
+
+    fn not(self, a: Self) -> Instruction {
+        panic!("not not supported for {}", Self::desc())
+    }
+
+    fn eqz(dest: RegSecretBit, a: Self) -> Instruction {
+        panic!("eqz not supported for {}", Self::desc())
     }
 }
 
@@ -64,6 +99,10 @@ impl RegType for RegSecretBit {
     }
 
     fn ld(self, imm: Imm) -> Instruction { instr::ldsbit(0, self, imm) }
+    fn and(self, a: Self, b: Self) -> Instruction { instr::andsb(0, self, a, b) }
+    fn or(self, a: Self, b: Self) -> Instruction { instr::orsb(0, self, a, b) }
+    fn xor(self, a: Self, b: Self) -> Instruction { instr::xorsb(0, self, a, b) }
+    fn not(self, a: Self) -> Instruction { instr::negb(0, self, a) }
 }
 
 impl RegType for RegSecretModp {
@@ -84,6 +123,52 @@ impl RegType for RegSecretRegint {
     }
 
     fn ld(self, imm: Imm) -> Instruction { instr::ldsint(0, self, imm) }
+    fn add(self, a: Self, b: Self) -> Instruction { instr::addsint(0, self, a, b) }
+    fn sub(self, a: Self, b: Self) -> Instruction { instr::subsint(0, self, a, b) }
+    fn mul(self, a: Self, b: Self) -> Instruction { instr::mulsint(0, self, a, b) }
+    fn neg(self, a: Self) -> Instruction { instr::neg(0, self, a) }
+    fn and(self, a: Self, b: Self) -> Instruction { instr::andsint(0, self, a, b) }
+    fn or(self, a: Self, b: Self) -> Instruction { instr::orsint(0, self, a, b) }
+    fn xor(self, a: Self, b: Self) -> Instruction { instr::xorsint(0, self, a, b) }
+    fn eqz(dest: RegSecretBit, a: Self) -> Instruction { instr::eqzsint(0, dest, a) }
+}
+
+
+pub trait Mux {
+    fn mux(b: &mut Builder, cond: RegSecretBit, then: Self, else_: Self) -> Self;
+}
+
+impl Mux for RegSecretRegint {
+    fn mux(b: &mut Builder, cond: RegSecretBit, then: Self, else_: Self) -> Self {
+        // `cond` as a SecretRegint
+        let cond_sr = b.sb_to_sr(cond);
+        // `mask` is all-ones if `cond` is 1, otherwise zero.
+        let cond_mask = b.neg(cond_sr);
+
+        let one = b.ld(1);
+        let not_cond_sr = b.sub(one, cond_sr);
+        let not_cond_mask = b.neg(not_cond_sr);
+
+        let then_masked = b.and(then, cond_mask);
+        let else_masked = b.and(else_, not_cond_mask);
+        b.or(then_masked, else_masked)
+    }
+}
+
+impl Mux for RegSecretBit {
+    fn mux(b: &mut Builder, cond: RegSecretBit, then: Self, else_: Self) -> Self {
+        let then_masked = b.and(then, cond);
+        let not_cond = b.not(cond);
+        let else_masked = b.and(else_, not_cond);
+        b.or(then_masked, else_masked)
+    }
+}
+
+impl<T: Mux> Mux for Vec<T> {
+    fn mux(b: &mut Builder, cond: RegSecretBit, then: Self, else_: Self) -> Self {
+        assert!(then.len() == else_.len(), "can't mux vectors of unequal len");
+        then.into_iter().zip(else_.into_iter()).map(|(x, y)| b.mux(cond, x, y)).collect()
+    }
 }
 
 
@@ -125,5 +210,103 @@ impl Builder {
 
     pub fn print<R: RegType>(&mut self, r: R) {
         self.emit(r.print());
+    }
+
+    pub fn add<R: RegType>(&mut self, r1: R, r2: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.add(r1, r2)))
+    }
+
+    pub fn sub<R: RegType>(&mut self, r1: R, r2: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.sub(r1, r2)))
+    }
+
+    pub fn mul<R: RegType>(&mut self, r1: R, r2: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.mul(r1, r2)))
+    }
+
+    pub fn neg<R: RegType>(&mut self, r: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.neg(r)))
+    }
+
+    pub fn and<R: RegType>(&mut self, r1: R, r2: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.and(r1, r2)))
+    }
+
+    pub fn or<R: RegType>(&mut self, r1: R, r2: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.or(r1, r2)))
+    }
+
+    pub fn xor<R: RegType>(&mut self, r1: R, r2: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.xor(r1, r2)))
+    }
+
+    pub fn not<R: RegType>(&mut self, r: R) -> R {
+        self.with_reg(|b, dest: R| b.emit(dest.not(r)))
+    }
+
+    pub fn eqz<R: RegType>(&mut self, r1: R) -> RegSecretBit {
+        self.with_reg(|b, dest| b.emit(R::eqz(dest, r1)))
+    }
+
+    pub fn eq<R: RegType>(&mut self, r1: R, r2: R) -> RegSecretBit {
+        let diff = self.sub(r1, r2);
+        self.eqz(diff)
+    }
+
+    pub fn sb_to_sr(&mut self, r: RegSecretBit) -> RegSecretRegint {
+        let zero = self.ld(0);
+        self.with_reg(|b, dest| b.emit(instr::sintsbit(0, dest, zero, r, Imm::new(0))))
+    }
+
+    // TODO: generalize
+    pub fn reveal(&mut self, r: RegSecretRegint) -> RegClearRegint {
+        self.with_reg(|b, dest| b.emit(instr::opensint(0, dest, r)))
+    }
+
+
+    pub fn mux<T: Mux>(&mut self, cond: RegSecretBit, then: T, else_: T) -> T {
+        T::mux(self, cond, then, else_)
+    }
+
+    pub fn match_<T: Mux>(
+        &mut self,
+        scrutinee: RegSecretRegint,
+        opts: impl Iterator<Item = usize>,
+        default: T,
+        mut f: impl FnMut(&mut Self, usize) -> T,
+    ) -> T {
+        let mut val = None;
+        for opt in opts {
+            let opt_sr = self.ld(opt as u32);
+            let chosen = self.eq(scrutinee, opt_sr);
+
+            let cur = f(self, opt);
+            val = Some(match val {
+                None => cur,
+                Some(prev) => self.mux(chosen, cur, prev),
+            });
+        }
+
+        val.unwrap_or_else(|| panic!("can't index in an empty collection"))
+    }
+
+    /// Index a collection with a secret regint, producing a secret result.
+    pub fn index<T: Copy + Mux>(
+        &mut self,
+        arr: &[T],
+        idx: RegSecretRegint,
+    ) -> T {
+        let mut val = None;
+        for (i, &cur) in arr.iter().enumerate() {
+            let i_sr = self.ld(i as u32);
+            let chosen = self.eq(idx, i_sr);
+
+            val = Some(match val {
+                None => cur,
+                Some(prev) => self.mux(chosen, cur, prev),
+            });
+        }
+
+        val.unwrap_or_else(|| panic!("can't index in an empty collection"))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,209 @@
 use std::fs::File;
 use std::io::{self, BufWriter};
 use scale_isa;
-use scale_isa::types::RegClearModp;
+use scale_isa::types::{RegClearModp, RegSecretRegint, RegSecretBit};
 
-use cheesecloth::builder::Builder;
+use cheesecloth::builder::{Builder, Mux};
 
+
+/// A TinyRAM instruction.  The program itself is not secret, but we most commonly load
+/// instructions from secret indices, which results in a secret instruction.
+#[derive(Clone, Copy)]
+struct RamInstr {
+    opcode: RegSecretRegint,
+    dest: RegSecretRegint,
+    op1: RegSecretRegint,
+    op2: RegSecretRegint,
+    /// Some opcodes have an operand that can be either a register name or an immediate value.  If
+    /// `imm` is set, it's interpreted as an immediate; otherwise, it's a register.
+    imm: RegSecretBit,
+}
+
+impl RamInstr {
+    pub fn new(
+        b: &mut Builder,
+        opcode: Opcode,
+        dest: u32,
+        op1: u32,
+        op2: u32,
+        imm: bool,
+    ) -> RamInstr {
+        RamInstr {
+            opcode: b.ld(opcode as u32),
+            dest: b.ld(dest),
+            op1: b.ld(op1),
+            op2: b.ld(op2),
+            imm: b.ld(imm as u32),
+        }
+    }
+
+    pub fn mov(b: &mut Builder, rd: u32, r1: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Mov, rd, r1, 0, false)
+    }
+
+    pub fn mov_imm(b: &mut Builder, rd: u32, imm: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Mov, rd, imm, 0, true)
+    }
+
+    pub fn add(b: &mut Builder, rd: u32, r1: u32, r2: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Add, rd, r1, r2, false)
+    }
+
+    pub fn add_imm(b: &mut Builder, rd: u32, r1: u32, imm: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Add, rd, r1, imm, true)
+    }
+
+    pub fn sub(b: &mut Builder, rd: u32, r1: u32, r2: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Sub, rd, r1, r2, false)
+    }
+
+    pub fn sub_imm(b: &mut Builder, rd: u32, r1: u32, imm: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Sub, rd, r1, imm, true)
+    }
+
+    pub fn mull(b: &mut Builder, rd: u32, r1: u32, r2: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Mull, rd, r1, r2, false)
+    }
+
+    pub fn mull_imm(b: &mut Builder, rd: u32, r1: u32, imm: u32) -> RamInstr {
+        RamInstr::new(b, Opcode::Mull, rd, r1, imm, true)
+    }
+}
+
+impl Mux for RamInstr {
+    fn mux(b: &mut Builder, cond: RegSecretBit, then: Self, else_: Self) -> Self {
+        RamInstr {
+            opcode: b.mux(cond, then.opcode, else_.opcode),
+            dest: b.mux(cond, then.dest, else_.dest),
+            op1: b.mux(cond, then.op1, else_.op1),
+            op2: b.mux(cond, then.op2, else_.op2),
+            imm: b.mux(cond, then.imm, else_.imm),
+        }
+    }
+}
+
+const RAM_REGS: usize = 3;
+
+struct RamState {
+    pc: RegSecretRegint,
+    regs: [RegSecretRegint; RAM_REGS],
+    flag: RegSecretBit,
+}
+
+impl RamState {
+    pub fn new(b: &mut Builder, pc: u32, regs: [u32; RAM_REGS], flag: bool) -> RamState {
+        let mut state_regs = [RegSecretRegint::new(0); RAM_REGS];
+        for (i, &v) in regs.iter().enumerate() {
+            state_regs[i] = b.ld(v);
+        }
+
+        RamState {
+            pc: b.ld(pc),
+            regs: state_regs,
+            flag: b.ld(flag as u32),
+        }
+    }
+}
+
+macro_rules! mk_opcode {
+    ($($Variant:ident,)*) => {
+        #[repr(u8)]
+        enum Opcode {
+            $($Variant,)*
+        }
+
+        impl Opcode {
+            fn from_raw(x: u8) -> Opcode {
+                let mut y = x;
+                $(
+                    if y == 0 { return Opcode::$Variant; }
+                    y -= 1;
+                )*
+                panic!("bad value {} for Opcode", x)
+            }
+
+            fn count() -> usize {
+                0 $(+ { drop(Opcode::$Variant); 1})*
+            }
+
+            fn iter() -> impl Iterator<Item = Opcode> {
+                (0 .. Self::count() as u8).map(Self::from_raw)
+            }
+        }
+    };
+}
+
+mk_opcode! {
+    Mov,
+    Add,
+    Sub,
+    Mull,
+}
+
+fn check_step(b: &mut Builder, prog: &[RamInstr], s1: &RamState, s2: &RamState) -> RegSecretBit {
+    let instr = b.index(prog, s1.pc);
+    let false_ = b.ld(0);
+    b.match_(
+        instr.opcode,
+        Opcode::iter().map(|i| i as usize),
+        false_,
+        |b, i| match Opcode::from_raw(i as u8) {
+            Opcode::Mov => {
+                b.ld(0)
+            },
+            Opcode::Add => {
+                b.ld(0)
+            },
+            Opcode::Sub => {
+                b.ld(0)
+            },
+            Opcode::Mull => {
+                b.ld(0)
+            },
+        },
+    )
+}
+
+fn check_last(b: &mut Builder, prog: &[RamInstr], s: &RamState) -> RegSecretBit {
+    let expect_pc = b.ld(prog.len() as u32);
+    let pc_ok = b.eq(s.pc, expect_pc);
+    let expect_r0 = b.ld(0);
+    let r0_ok = b.eq(s.regs[0], expect_r0);
+    b.and(pc_ok, r0_ok)
+}
 
 fn main() -> io::Result<()> {
     let mut b = Builder::default();
 
     {
-        let r = b.ld::<RegClearModp>(1234);
-        b.print(r);
+        let prog = vec![
+            RamInstr::mull(&mut b, 2, 0, 0),        // r2 = r0 * r0 (x^2)
+            RamInstr::mull_imm(&mut b, 1, 0, 3),    // r1 = 3 * r0 (3x)
+            RamInstr::add(&mut b, 0, 1, 2),         // r0 = r1 + r2 (x^2 + 3x)
+            RamInstr::sub_imm(&mut b, 0, 0, 10),    // r0 = r0 - 10 (x^2 + 3x - 10)
+        ];
+
+        let trace = vec![
+            RamState::new(&mut b, 0, [2, 0, 0], false),
+            RamState::new(&mut b, 1, [2, 0, 4], false),
+            RamState::new(&mut b, 2, [2, 6, 4], false),
+            RamState::new(&mut b, 3, [10, 6, 4], false),
+            RamState::new(&mut b, 4, [0, 6, 4], false),
+        ];
+
+        let mut ok: RegSecretBit = b.ld(1);
+
+        for (s1, s2) in trace.iter().zip(trace.iter().skip(1)) {
+            let step_ok = check_step(&mut b, &prog, s1, s2);
+            ok = b.and(ok, step_ok);
+        }
+
+        let last_ok = check_last(&mut b, &prog, trace.last().unwrap());
+        ok = b.and(ok, last_ok);
+
+        let ok_sr = b.sb_to_sr(ok);
+        let ok_clear = b.reveal(ok_sr);
+        b.print(ok_clear);
     }
 
     let instrs = b.finish();
