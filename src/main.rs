521df442efc2ba5b9ffe51217117cd06d357bccb
Stuart Pernsteiner <spernsteiner@galois.com>
2020-05-28 09:50:22 -0700

    working trace checker prototype

--- src/main.rs
+++ src/main.rs
@@ -140,36 +140,76 @@ mk_opcode! {
     Mull,
 }
 
-fn check_step(b: &mut Builder, prog: &[RamInstr], s1: &RamState, s2: &RamState) -> RegSecretBit {
+fn operand_value(
+    b: &mut Builder,
+    s: &RamState,
+    op: RegSecretRegint,
+    imm: RegSecretBit,
+) -> RegSecretRegint {
+    let reg_val = b.index(&s.regs, op);
+    b.mux(imm, op, reg_val)
+}
+
+fn check_step(b: &mut Builder, prog: &[RamInstr], s1: &RamState, s2: &RamState) -> Vec<RegSecretBit> {
     let instr = b.index(prog, s1.pc);
     let false_ = b.ld(0);
     b.match_(
         instr.opcode,
         Opcode::iter().map(|i| i as usize),
-        false_,
-        |b, i| match Opcode::from_raw(i as u8) {
-            Opcode::Mov => {
-                b.ld(0)
-            },
-            Opcode::Add => {
-                b.ld(0)
-            },
-            Opcode::Sub => {
-                b.ld(0)
-            },
-            Opcode::Mull => {
-                b.ld(0)
-            },
+        vec![false_; RAM_REGS],
+        |b, i| {
+            let result = match Opcode::from_raw(i as u8) {
+                Opcode::Mov => {
+                    operand_value(b, s1, instr.op1, instr.imm)
+                },
+                Opcode::Add => {
+                    let x = b.index(&s1.regs, instr.op1);
+                    let y = operand_value(b, s1, instr.op2, instr.imm);
+                    b.add(x, y)
+                },
+                Opcode::Sub => {
+                    let x = b.index(&s1.regs, instr.op1);
+                    let y = operand_value(b, s1, instr.op2, instr.imm);
+                    b.sub(x, y)
+                },
+                Opcode::Mull => {
+                    let x = b.index(&s1.regs, instr.op1);
+                    let y = operand_value(b, s1, instr.op2, instr.imm);
+                    b.mul(x, y)
+                },
+            };
+
+            let mut ok = Vec::new();
+            for (i, (&v_old, &v_new)) in s1.regs.iter().zip(s2.regs.iter()).enumerate() {
+                let i_val = b.ld(i as u32);
+                let is_dest = b.eq(i_val, instr.dest);
+                let expect_new = b.mux(is_dest, result, v_old);
+                let new_ok = b.eq(v_new, expect_new);
+                ok.push(new_ok);
+            }
+            ok
         },
     )
 }
 
-fn check_last(b: &mut Builder, prog: &[RamInstr], s: &RamState) -> RegSecretBit {
+fn check_first(b: &mut Builder, prog: &[RamInstr], s: &RamState) -> Vec<RegSecretBit> {
+    let mut ok = Vec::new();
+    let expect_pc = b.ld(0);
+    ok.push(b.eq(s.pc, expect_pc));
+    for &r in s.regs.iter().skip(1) {
+        let expect_val = b.ld(0);
+        ok.push(b.eq(r, expect_val));
+    }
+    ok.push(b.not(s.flag));
+    ok
+}
+
+fn check_last(b: &mut Builder, prog: &[RamInstr], s: &RamState) -> Vec<RegSecretBit> {
     let expect_pc = b.ld(prog.len() as u32);
     let pc_ok = b.eq(s.pc, expect_pc);
     let expect_r0 = b.ld(0);
     let r0_ok = b.eq(s.regs[0], expect_r0);
-    b.and(pc_ok, r0_ok)
+    vec![pc_ok, r0_ok]
 }
 
 fn main() -> io::Result<()> {
@@ -191,19 +231,21 @@ fn main() -> io::Result<()> {
             RamState::new(&mut b, 4, [0, 6, 4], false),
         ];
 
-        let mut ok: RegSecretBit = b.ld(1);
+        let mut ok = Vec::new();
+
+        ok.append(&mut check_first(&mut b, &prog, trace.first().unwrap()));
 
         for (s1, s2) in trace.iter().zip(trace.iter().skip(1)) {
-            let step_ok = check_step(&mut b, &prog, s1, s2);
-            ok = b.and(ok, step_ok);
+            ok.append(&mut check_step(&mut b, &prog, s1, s2));
         }
 
-        let last_ok = check_last(&mut b, &prog, trace.last().unwrap());
-        ok = b.and(ok, last_ok);
+        ok.append(&mut check_last(&mut b, &prog, trace.last().unwrap()));
 
-        let ok_sr = b.sb_to_sr(ok);
-        let ok_clear = b.reveal(ok_sr);
-        b.print(ok_clear);
+        for sb in ok {
+            let sr = b.sb_to_sr(sb);
+            let clear = b.reveal(sr);
+            b.print(clear);
+        }
     }
 
     let instrs = b.finish();
