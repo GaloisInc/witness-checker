748d1cefda1c853112b2296cc237b7539d8fbd5d
Stuart Pernsteiner <spernsteiner@galois.com>
2020-06-12 11:38:28 -0700

    refactor: remove Secrecy argument from TypedWire

--- src/ir/typed.rs
+++ src/ir/typed.rs
@@ -3,26 +3,11 @@ use std::mem::MaybeUninit;
 use crate::ir::builder::{self, Builder};
 use crate::ir::circuit::{Wire, Ty, TyKind};
 
-pub trait Secrecy {
-    const SECRET: bool;
-}
-
-pub struct Secret;
-pub struct Public;
-
-impl Secrecy for Secret {
-    const SECRET: bool = true;
-}
-
-impl Secrecy for Public {
-    const SECRET: bool = false;
-}
-
 
 pub trait Repr<'a> {
     type Repr;
 
-    fn lit<S: Secrecy>(bld: &Builder<'a>, x: Self) -> Self::Repr;
+    fn lit(bld: &Builder<'a>, x: Self) -> Self::Repr;
 }
 
 macro_rules! repr_tuple_impl {
@@ -30,11 +15,11 @@ macro_rules! repr_tuple_impl {
         impl<'a, $($A: Repr<'a>,)*> Repr<'a> for ($($A,)*) {
             type Repr = ($(<$A as Repr<'a>>::Repr,)*);
 
-            fn lit<S: Secrecy>(bld: &Builder<'a>, a: Self) -> Self::Repr {
+            fn lit(bld: &Builder<'a>, a: Self) -> Self::Repr {
                 #![allow(bad_style)]    // Capitalized variable names $A
                 #![allow(unused)]       // `bld` in the zero-element case
                 let ($($A,)*) = a;
-                ($($A::lit::<S>(bld, $A),)*)
+                ($($A::lit(bld, $A),)*)
             }
         }
     };
@@ -58,7 +43,7 @@ macro_rules! repr_array_impls {
             impl<'a, A: Repr<'a>> Repr<'a> for [A; $n] {
                 type Repr = [A::Repr; $n];
 
-                fn lit<S: Secrecy>(bld: &Builder<'a>, a: Self) -> Self::Repr {
+                fn lit(bld: &Builder<'a>, a: Self) -> Self::Repr {
                     // Can't `collect()` or `into_iter()` an array yet, which makes this difficult
                     // to implement without unnecessary allocation.
                     unsafe {
@@ -68,7 +53,7 @@ macro_rules! repr_array_impls {
                         for i in 0 .. $n {
                             let a_val = (a.as_ptr() as *const A).add(i).read();
                             // If this panics, the remaining elements of `a` and `b` will leak.
-                            let o_val = Repr::lit::<S>(bld, a_val);
+                            let o_val = Repr::lit(bld, a_val);
 
                             (o.as_mut_ptr() as *mut A::Repr).add(i).write(o_val);
                         }
@@ -91,54 +76,30 @@ repr_array_impls!(
 impl<'a, A: Repr<'a>> Repr<'a> for Vec<A> {
     type Repr = Vec<A::Repr>;
 
-    fn lit<S: Secrecy>(bld: &Builder<'a>, a: Self) -> Self::Repr {
-        a.into_iter().map(|a_val| A::lit::<S>(bld, a_val)).collect::<Vec<_>>()
+    fn lit(bld: &Builder<'a>, a: Self) -> Self::Repr {
+        a.into_iter().map(|a_val| A::lit(bld, a_val)).collect::<Vec<_>>()
     }
 }
 
 
-pub struct TypedWire<'a, S: Secrecy, T: Repr<'a>> {
+pub struct TypedWire<'a, T: Repr<'a>> {
     repr: T::Repr,
-    _marker: PhantomData<S>,
 }
 
-impl<'a, S: Secrecy, T: Repr<'a>> TypedWire<'a, S, T> {
-    pub fn new(repr: T::Repr) -> TypedWire<'a, S, T> {
+impl<'a, T: Repr<'a>> TypedWire<'a, T> {
+    pub fn new(repr: T::Repr) -> TypedWire<'a, T> {
         TypedWire {
             repr,
-            _marker: PhantomData,
         }
     }
 }
 
-pub type SWire<'a, T> = TypedWire<'a, Secret, T>;
-pub type PWire<'a, T> = TypedWire<'a, Public, T>;
+pub type SWire<'a, T> = TypedWire<'a, T>;
+pub type PWire<'a, T> = TypedWire<'a, T>;
 
 type ReprOf<'a, T> = <T as Repr<'a>>::Repr;
 
 
-
-macro_rules! impl_unary_op1 {
-    ($Op:ident :: $op:ident ($S1:ident) -> $SR:ident) => {
-        impl<'a, X> builder::$Op<'a> for TypedWire<'a, $S1, X>
-        where
-            X: Repr<'a> + $Op<'a>,
-            ReprOf<'a, X>: builder::$Op<
-                'a,
-                Output = ReprOf<'a, <X as $Op<'a>>::Output>,
-            >,
-        {
-            type Output = TypedWire<'a, $SR, <X as $Op<'a>>::Output>;
-
-            fn $op(
-                bld: &Builder<'a>,
-                x: TypedWire<'a, $S1, X>,
-            ) -> TypedWire<'a, $SR, <X as $Op<'a>>::Output> {
-                TypedWire::new(builder::$Op::$op(bld, x.repr))
-            }
-        }
-    };
-}
 
 macro_rules! define_unary_op {
     ($Op:ident :: $op:ident) => {
@@ -156,34 +117,21 @@ macro_rules! define_unary_op {
             type Output: Repr<'a>;
         }
 
-        impl_unary_op1!($Op::$op(Secret) -> Secret);
-        impl_unary_op1!($Op::$op(Public) -> Public);
-    };
-}
-
-macro_rules! impl_binary_op1 {
-    ($Op:ident :: $op:ident ($S1:ident, $S2:ident) -> $SR:ident) => {
-        impl<'a, X, Y> builder::$Op<
-            'a,
-            TypedWire<'a, $S2, Y>,
-        > for TypedWire<'a, $S1, X>
+        impl<'a, X> builder::$Op<'a> for TypedWire<'a, X>
         where
-            X: Repr<'a> + $Op<'a, Y>,
-            Y: Repr<'a>,
+            X: Repr<'a> + $Op<'a>,
             ReprOf<'a, X>: builder::$Op<
                 'a,
-                ReprOf<'a, Y>,
-                Output = ReprOf<'a, <X as $Op<'a, Y>>::Output>,
+                Output = ReprOf<'a, <X as $Op<'a>>::Output>,
             >,
         {
-            type Output = TypedWire<'a, $SR, <X as $Op<'a, Y>>::Output>;
+            type Output = TypedWire<'a, <X as $Op<'a>>::Output>;
 
             fn $op(
                 bld: &Builder<'a>,
-                x: TypedWire<'a, $S1, X>,
-                y: TypedWire<'a, $S2, Y>,
-            ) -> TypedWire<'a, $SR, <X as $Op<'a, Y>>::Output> {
-                TypedWire::new(builder::$Op::$op(bld, x.repr, y.repr))
+                x: TypedWire<'a, X>,
+            ) -> TypedWire<'a, <X as $Op<'a>>::Output> {
+                TypedWire::new(builder::$Op::$op(bld, x.repr))
             }
         }
     };
@@ -206,41 +154,27 @@ macro_rules! define_binary_op {
             type Output: Repr<'a>;
         }
 
-        impl_binary_op1!($Op::$op(Secret, Secret) -> Secret);
-        impl_binary_op1!($Op::$op(Public, Secret) -> Secret);
-        impl_binary_op1!($Op::$op(Secret, Public) -> Secret);
-        impl_binary_op1!($Op::$op(Public, Public) -> Public);
-    };
-}
-
-// Mux is a special case due to the unusual order of its type vs. value arguments.
-macro_rules! impl_mux1 {
-    ($Op:ident :: $op:ident ($SC:ident, $ST:ident, $SE:ident) -> $SR:ident) => {
-        impl<'a, C, T, E> builder::$Op<
+        impl<'a, X, Y> builder::$Op<
             'a,
-            TypedWire<'a, $SC, C>,
-            TypedWire<'a, $SE, E>,
-        > for TypedWire<'a, $ST, T>
+            TypedWire<'a, Y>,
+        > for TypedWire<'a, X>
         where
-            C: Repr<'a>,
-            T: Repr<'a> + $Op<'a, C, E>,
-            E: Repr<'a>,
-            ReprOf<'a, T>: builder::$Op<
+            X: Repr<'a> + $Op<'a, Y>,
+            Y: Repr<'a>,
+            ReprOf<'a, X>: builder::$Op<
                 'a,
-                ReprOf<'a, C>,
-                ReprOf<'a, E>,
-                Output = ReprOf<'a, <T as $Op<'a, C, E>>::Output>,
+                ReprOf<'a, Y>,
+                Output = ReprOf<'a, <X as $Op<'a, Y>>::Output>,
             >,
         {
-            type Output = TypedWire<'a, $SR, <T as $Op<'a, C, E>>::Output>;
+            type Output = TypedWire<'a, <X as $Op<'a, Y>>::Output>;
 
             fn $op(
                 bld: &Builder<'a>,
-                c: TypedWire<'a, $SC, C>,
-                t: TypedWire<'a, $ST, T>,
-                e: TypedWire<'a, $SE, E>,
-            ) -> TypedWire<'a, $SR, <T as $Op<'a, C, E>>::Output> {
-                TypedWire::new(builder::$Op::$op(bld, c.repr, t.repr, e.repr))
+                x: TypedWire<'a, X>,
+                y: TypedWire<'a, Y>,
+            ) -> TypedWire<'a, <X as $Op<'a, Y>>::Output> {
+                TypedWire::new(builder::$Op::$op(bld, x.repr, y.repr))
             }
         }
     };
@@ -269,14 +203,33 @@ macro_rules! define_mux {
             type Output: Repr<'a>;
         }
 
-        impl_mux1!($Op::$op(Secret, Secret, Secret) -> Secret);
-        impl_mux1!($Op::$op(Public, Secret, Secret) -> Secret);
-        impl_mux1!($Op::$op(Secret, Public, Secret) -> Secret);
-        impl_mux1!($Op::$op(Public, Public, Secret) -> Secret);
-        impl_mux1!($Op::$op(Secret, Secret, Public) -> Secret);
-        impl_mux1!($Op::$op(Public, Secret, Public) -> Secret);
-        impl_mux1!($Op::$op(Secret, Public, Public) -> Secret);
-        impl_mux1!($Op::$op(Public, Public, Public) -> Public);
+        impl<'a, C, T, E> builder::$Op<
+            'a,
+            TypedWire<'a, C>,
+            TypedWire<'a, E>,
+        > for TypedWire<'a, T>
+        where
+            C: Repr<'a>,
+            T: Repr<'a> + $Op<'a, C, E>,
+            E: Repr<'a>,
+            ReprOf<'a, T>: builder::$Op<
+                'a,
+                ReprOf<'a, C>,
+                ReprOf<'a, E>,
+                Output = ReprOf<'a, <T as $Op<'a, C, E>>::Output>,
+            >,
+        {
+            type Output = TypedWire<'a, <T as $Op<'a, C, E>>::Output>;
+
+            fn $op(
+                bld: &Builder<'a>,
+                c: TypedWire<'a, C>,
+                t: TypedWire<'a, T>,
+                e: TypedWire<'a, E>,
+            ) -> TypedWire<'a, <T as $Op<'a, C, E>>::Output> {
+                TypedWire::new(builder::$Op::$op(bld, c.repr, t.repr, e.repr))
+            }
+        }
     };
 }
 
@@ -306,8 +259,8 @@ macro_rules! define_prim_reprs {
         $(
             impl<'a> Repr<'a> for $T {
                 type Repr = Wire<'a>;
-                fn lit<S: Secrecy>(bld: &Builder<'a>, x: $T) -> Wire<'a> {
-                    bld.circuit().lit(Ty::new(TyKind::$TyKind, S::SECRET), x as u64)
+                fn lit(bld: &Builder<'a>, x: $T) -> Wire<'a> {
+                    bld.circuit().lit(Ty::new(TyKind::$TyKind, false), x as u64)
                 }
             }
 
