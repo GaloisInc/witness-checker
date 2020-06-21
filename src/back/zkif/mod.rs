83b048ee6d43402f82a47bdae88b1a8a20100d8a
aurel <aurel@qed-it.com>
2020-06-21 02:00:13 +0200

    zkif: allocate zkif variables for field/bits repr of wires

--- src/back/zkif/mod.rs
+++ src/back/zkif/mod.rs
@@ -8,28 +8,31 @@ use std::collections::HashMap;
 type OpCode = usize;
 type RegLabel = usize;
 
-type WireId = u64;
-type WireValue = [u64; 4];
-
 #[derive(Copy, Clone)]
-pub struct WirePack(WireId);
+pub struct WireId(usize); // or wid.
+
+type ZkifId = u64; // or zid.
+
+struct WireRepr {
+    packed_zid: Option<ZkifId>,
+    bit_zids: Vec<ZkifId>,
+}
 
 
 pub struct Backend {
-    free_var_id: u64,
+    wire_reprs: Vec<WireRepr>,
+    free_zid: ZkifId,
 
-    cost: usize,
+    pub cost: usize,
     last_printed_cost: usize,
 }
 
 impl Backend {
-    pub fn new() -> Backend { Backend { free_var_id: 1, cost: 0, last_printed_cost: 0 } }
+    pub fn new() -> Backend { Backend { free_zid: 1, wire_reprs: vec![], cost: 0, last_printed_cost: 0 } }
 
-    pub fn allocate(&mut self) -> WirePack {
-        let new_id = self.free_var_id;
-        self.free_var_id += 1;
-        self.cost += 1 + 16; // Word size, boolean-ness.
-        return WirePack(new_id);
+    pub fn allocate(&mut self) -> WireId {
+        self.wire_reprs.push(WireRepr { packed_zid: None, bit_zids: vec![] });
+        return WireId(self.wire_reprs.len() - 1);
     }
 
     pub fn print_cost(&mut self) {
@@ -41,7 +44,7 @@ impl Backend {
 // Core execution units.
 impl Backend {
     // new wire = operation(v0 v1 v2)
-    pub fn push_operation(&mut self, opcode: OpCode, regval0: WirePack, regval1: WirePack, regval2: WirePack) -> WirePack {
+    pub fn push_operation(&mut self, opcode: OpCode, regval0: WireId, regval1: WireId, regval2: WireId) -> WireId {
         // TODO: use the implementation for the given opcode.
         let regout = self.allocate();
         self.cost += 30;
@@ -50,7 +53,7 @@ impl Backend {
     }
 
     // new wire = inputs[index]
-    pub fn push_demux(&mut self, inputs: &[WirePack], index: WirePack) -> WirePack {
+    pub fn push_demux(&mut self, inputs: &[WireId], index: WireId) -> WireId {
         // TODO: use secret index.
         let regout = self.allocate();
         self.cost += 1 + inputs.len();
@@ -60,7 +63,7 @@ impl Backend {
 
     // regs[index] = new wire equal to new_value.
     // regs[i] = new wire equal to regs[i], for i != index.
-    pub fn push_update(&mut self, regs: &mut [WirePack], index: WirePack, new_value: WirePack) {
+    pub fn push_update(&mut self, regs: &mut [WireId], index: WireId, new_value: WireId) {
         for i in 0..regs.len() {
             // TODO: condition on secret index.
             regs[i] = self.allocate();
@@ -79,15 +82,15 @@ pub struct FixedInstr {
 
 pub struct SecretInstr<'a> {
     possible_opcodes: &'a [OpCode],
-    opcode: WirePack,
-    reglabel0: WirePack,
-    reglabel1: WirePack,
-    reglabel2: WirePack,
+    opcode: WireId,
+    reglabel0: WireId,
+    reglabel1: WireId,
+    reglabel2: WireId,
 }
 
 // CPU components. Compose and connect core components.
 impl Backend {
-    pub fn push_instr(&mut self, regs: &mut [WirePack], instr: &FixedInstr) {
+    pub fn push_instr(&mut self, regs: &mut [WireId], instr: &FixedInstr) {
         println!("instruction operation_{}( {}, {}, {} )", instr.opcode, instr.reglabel0, instr.reglabel1, instr.reglabel2);
         let result = self.push_operation(
             instr.opcode,
@@ -99,7 +102,7 @@ impl Backend {
         self.print_cost();
     }
 
-    pub fn push_secret_instr(&mut self, regvals: &mut [WirePack], instr: &SecretInstr) {
+    pub fn push_secret_instr(&mut self, regvals: &mut [WireId], instr: &SecretInstr) {
         println!("secret instruction {:?}( {:?}, {:?}, {:?} )", instr.opcode, instr.reglabel0, instr.reglabel1, instr.reglabel2);
         println!("// Pick the register inputs from all possible registers.");
         let regval0 = self.push_demux(regvals, instr.reglabel0);
@@ -110,7 +113,7 @@ impl Backend {
         println!("// Execute all possible operations.");
         let possible_results = instr.possible_opcodes.iter().map(|op|
             self.push_operation(*op, regval0, regval1, regval2)
-        ).collect::<Vec<WirePack>>();
+        ).collect::<Vec<WireId>>();
         self.print_cost();
 
         println!("// Pick the result of the actual operation.");
@@ -121,16 +124,53 @@ impl Backend {
     }
 }
 
+// Wire representations in zkInterface.
+impl Backend {
+    fn get_field_repr(&mut self, wid: WireId) -> ZkifId {
+        let wire = &mut self.wire_reprs[wid.0];
+        match wire.packed_zid {
+            Some(zid) => zid,
+            None => {
+                let zid = self.free_zid;
+                self.free_zid += 1;
+                wire.packed_zid = Some(zid);
+                self.cost += 1 + 16; // Word size, boolean-ness.
+                // TODO: if bits repr exists, enforce equality.
+                zid
+            }
+        }
+    }
+
+    fn get_bit_repr(&mut self, wid: WireId) -> &[ZkifId] {
+        let wire = &mut self.wire_reprs[wid.0];
+        if wire.bit_zids.len() == 0 {
+            let width = 16;
+            wire.bit_zids.resize(width, 0);
+            for bit_i in 0..width {
+                wire.bit_zids[bit_i] = self.free_zid + bit_i as u64;
+            }
+            self.free_zid += width as u64;
+            self.cost += 1 + width;
+            // TODO: enforce boolean-ness.
+            // TODO: if field repr exists, enforce equality.
+        }
+        return &wire.bit_zids;
+    }
+}
+
+
 pub struct Memory {
     ops: Vec<MemOp>,
-    values: HashMap<WireValue, WireValue>,
+    values: HashMap<PackedValue, PackedValue>,
     finished: bool,
 }
 
 enum MemOp {
-    Store { addr: WirePack, content: WirePack },
-    Load { addr: WirePack, content: WirePack },
+    Store { addr: WireId, content: WireId },
+    Load { addr: WireId, content: WireId },
 }
+
+type PackedValue = [u64; 4];
 
 impl Memory {
     pub fn new() -> Memory {
@@ -141,12 +181,12 @@ impl Memory {
         self.finished = true;
     }
 
-    pub fn store(&mut self, addr: WirePack, content: WirePack) {
+    pub fn store(&mut self, addr: WireId, content: WireId) {
         self.ops.push(MemOp::Store { addr, content });
         self.values.insert([0, 0, 0, 0], [0, 0, 0, 0]);
     }
 
-    pub fn load(&mut self, back: &mut Backend, addr: WirePack) -> WirePack {
+    pub fn load(&mut self, back: &mut Backend, addr: WireId) -> WireId {
         let content = back.allocate();
         self.ops.push(MemOp::Load { addr, content });
         // TODO: copy values[addr] into the new wire.
@@ -166,7 +206,7 @@ impl Drop for Memory {
 fn test_zkif_backend() {
     let mut back = Backend::new();
     let mut mem = Memory::new();
-    let mut regs = vec![WirePack(0); 4];
+    let mut regs = vec![WireId(0); 4];
     for reg in regs.iter_mut() {
         *reg = back.allocate();
     }
@@ -214,8 +254,8 @@ fn test_zkif_backend() {
 }
 
 
-impl fmt::Debug for WirePack {
+impl fmt::Debug for WireId {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        debug::write_var_name(self.0, f)
+        debug::write_wire_name(self.0, f)
     }
 }
