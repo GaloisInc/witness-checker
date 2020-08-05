aebf95673101e61c393335364415068bf691d986
Stuart Pernsteiner <spernsteiner@galois.com>
2020-08-05 10:05:24 -0700

    distinguish invalid traces from bugs in the subject program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::env;
 use std::fs::{self, File};
 use std::io::{self, BufWriter};
@@ -11,6 +12,75 @@ use cheesecloth::parse;
 use cheesecloth::sort;
 use cheesecloth::tiny_ram::{RamInstr, RamState, MemPort, Opcode, REG_NONE, REG_PC};
 
+struct Context<'a> {
+    asserts: RefCell<Vec<TWire<'a, bool>>>,
+    bugs: RefCell<Vec<TWire<'a, bool>>>,
+}
+
+impl<'a> Context<'a> {
+    fn new() -> Context<'a> {
+        Context {
+            asserts: RefCell::new(Vec::new()),
+            bugs: RefCell::new(Vec::new()),
+        }
+    }
+
+    fn finish(self) -> (Vec<TWire<'a, bool>>, Vec<TWire<'a, bool>>) {
+        (
+            self.asserts.into_inner(),
+            self.bugs.into_inner(),
+        )
+    }
+
+    /// Mark the execution as invalid if `cond` is false.  A failed assertion represents
+    /// misbehavior on the part of the prover.
+    fn assert(&self, cond: TWire<'a, bool>) {
+        self.asserts.borrow_mut().push(cond);
+    }
+
+    /// Signal an error condition of `cond` is true.  This should be used for situations like
+    /// buffer overflows, which indicate a bug in the subject program.
+    fn bug_if(&self, cond: TWire<'a, bool>) {
+        self.bugs.borrow_mut().push(cond);
+    }
+
+    fn when<R>(
+        &self,
+        b: &Builder<'a>,
+        path_cond: TWire<'a, bool>,
+        f: impl FnOnce(&ContextWhen<'a, '_>) -> R,
+    ) -> R {
+        f(&ContextWhen { cx: self, b, path_cond })
+    }
+}
+
+struct ContextWhen<'a, 'b> {
+    cx: &'b Context<'a>,
+    b: &'b Builder<'a>,
+    path_cond: TWire<'a, bool>,
+}
+
+impl<'a, 'b> ContextWhen<'a, 'b> {
+    fn assert(&self, cond: TWire<'a, bool>) {
+        // The assertion passes if either this `when` block is not taken, or `cond` is satisfied.
+        self.cx.assert(self.b.or(self.b.not(self.path_cond), cond));
+    }
+
+    fn bug_if(&self, cond: TWire<'a, bool>) {
+        // The bug occurs if this `when` block is taken and `cond` is satisfied.
+        self.cx.bug_if(self.b.and(self.path_cond, cond));
+    }
+
+    fn when<R>(
+        &self,
+        b: &Builder<'a>,
+        path_cond: TWire<'a, bool>,
+        f: impl FnOnce(&ContextWhen<'a, '_>) -> R,
+    ) -> R {
+        self.cx.when(b, b.and(self.path_cond, path_cond), f)
+    }
+}
+
 fn operand_value<'a>(
     b: &Builder<'a>,
     s: &TWire<'a, RamState>,
@@ -22,13 +92,14 @@ fn operand_value<'a>(
 }
 
 fn check_step<'a>(
+    cx: &Context<'a>,
     b: &Builder<'a>,
     cycle: u32,
     prog: &[TWire<'a, RamInstr>],
     mem_ports: &[TWire<'a, MemPort>],
     s1: &TWire<'a, RamState>,
     s2: &TWire<'a, RamState>,
-) -> Vec<TWire<'a, bool>> {
+) {
     let instr = b.index(prog, s1.pc, |b, i| b.lit(i as u64));
 
     let mut cases = Vec::new();
@@ -179,19 +250,17 @@ fn check_step<'a>(
 
     let (result, dest, expect_flag) = *b.mux_multi(&cases, b.lit((0, REG_NONE, false)));
 
-    let mut ok = Vec::new();
     for (i, (&v_old, &v_new)) in s1.regs.iter().zip(s2.regs.iter()).enumerate() {
         let is_dest = b.eq(b.lit(i as u64), dest);
         let expect_new = b.mux(is_dest, result, v_old);
-        let new_ok = b.eq(v_new, expect_new);
-        ok.push(new_ok);
+        cx.assert(b.eq(v_new, expect_new));
     }
 
     let pc_is_dest = b.eq(b.lit(REG_PC), dest);
     let expect_pc = b.mux(pc_is_dest, result, b.add(s1.pc, b.lit(1)));
-    ok.push(b.eq(s2.pc, expect_pc));
+    cx.assert(b.eq(s2.pc, expect_pc));
 
-    ok.push(b.eq(s2.flag, expect_flag));
+    cx.assert(b.eq(s2.flag, expect_flag));
 
 
     // If the instruction is a store or a load, we need additional checks to make sure the fields
@@ -206,70 +275,76 @@ fn check_step<'a>(
     let load_value_ok = b.eq(mem_port.value, result);
     let store_value_ok = b.eq(mem_port.value, x);
     let store_ok = b.eq(mem_port.write, is_store);
-    ok.push(b.mux(is_mem, cycle_ok, b.lit(true)));
-    ok.push(b.mux(is_mem, addr_ok, b.lit(true)));
-    ok.push(b.mux(is_load, load_value_ok, b.lit(true)));
-    ok.push(b.mux(is_store, store_value_ok, b.lit(true)));
-    ok.push(b.mux(is_mem, store_ok, b.lit(true)));
+    cx.assert(b.mux(is_mem, cycle_ok, b.lit(true)));
+    cx.assert(b.mux(is_mem, addr_ok, b.lit(true)));
+    cx.assert(b.mux(is_load, load_value_ok, b.lit(true)));
+    cx.assert(b.mux(is_store, store_value_ok, b.lit(true)));
+    cx.assert(b.mux(is_mem, store_ok, b.lit(true)));
 
     // Non-memory ops must not use a memory port.  This prevents a malicious prover from
     // introducing fake stores on non-store instructions.
-    ok.push(b.eq(has_mem_port, is_mem));
-
-
-    ok
+    cx.assert(b.eq(has_mem_port, is_mem));
 }
 
 fn check_first<'a>(
+    cx: &Context<'a>,
     b: &Builder<'a>,
     _prog: &[TWire<'a, RamInstr>],
     s: &TWire<'a, RamState>,
-) -> Vec<TWire<'a, bool>> {
-    let mut ok = Vec::new();
-    ok.push(b.eq(s.pc, b.lit(0)));
+) {
+    cx.assert(b.eq(s.pc, b.lit(0)));
     for &r in s.regs.iter().skip(1) {
-        ok.push(b.eq(r, b.lit(0)));
+        cx.assert(b.eq(r, b.lit(0)));
     }
-    ok.push(b.not(s.flag));
-    ok
+    cx.assert(b.not(s.flag));
 }
 
 fn check_last<'a>(
+    cx: &Context<'a>,
     b: &Builder<'a>,
     prog: &[TWire<'a, RamInstr>],
     s: &TWire<'a, RamState>,
-) -> Vec<TWire<'a, bool>> {
-    let pc_ok = b.eq(s.pc, b.lit(prog.len() as u64));
-    let r0_ok = b.eq(s.regs[0], b.lit(0));
-    vec![pc_ok, r0_ok]
+) {
+    cx.assert(b.eq(s.pc, b.lit(prog.len() as u64)));
+    cx.bug_if(b.ne(s.regs[0], b.lit(0)));
 }
 
 fn check_first_mem<'a>(
+    cx: &Context<'a>,
     b: &Builder<'a>,
     port: &TWire<'a, MemPort>,
-) -> Vec<TWire<'a, bool>> {
+) {
     // If the first memory port is active, then it must be a write, since there are no previous
     // writes to read from.
     let active = b.ne(port.cycle, b.lit(!0));
-    vec![b.mux(active, port.write, b.lit(true))]
+    cx.bug_if(b.mux(active, b.not(port.write), b.lit(false)));
 }
 
 fn check_mem<'a>(
+    cx: &Context<'a>,
     b: &Builder<'a>,
     port1: &TWire<'a, MemPort>,
     port2: &TWire<'a, MemPort>,
-) -> Vec<TWire<'a, bool>> {
+) {
     let active = b.and(b.ne(port1.cycle, b.lit(!0)), b.ne(port2.cycle, b.lit(!0)));
 
-    let val_ok = b.mux(
-        b.not(port2.write),
-        // If `port2` is a read, then `port1` must be a write with the same address and value.
-        b.and(b.eq(port1.addr, port2.addr), b.eq(port1.value, port2.value)),
-        // Otherwise, `port2` is unconstrained.
-        b.lit(true),
-    );
+    cx.when(b, active, |cx| {
+        cx.when(b, b.not(port2.write), |cx| {
+            // `port2` is a read.
 
-    vec![b.mux(active, val_ok, b.lit(true))]
+            // `port1` should be a read or write with the same address.  Otherwise, `port2` is a
+            // read from uninitialized memory.
+            let is_init = b.eq(port1.addr, port2.addr);
+            cx.bug_if(b.not(is_init));
+
+            // If this is a legal read, then it must return the same value as the previous
+            // operation.  Otherwise, the prover is cheating by changing memory without a proper
+            // write.
+            cx.when(b, is_init, |cx| cx.assert(b.eq(port1.value, port2.value)));
+        });
+
+        // If `port2` is a write, then its address and value are unconstrained.
+    });
 }
 
 fn main() -> io::Result<()> {
@@ -277,6 +352,7 @@ fn main() -> io::Result<()> {
     assert!(args.len() == 3, "usage: {} PROGRAM TRACE", args.get(0).map_or("witness-checker", |x| x));
     let arena = Bump::new();
     let b = Builder::new(&arena);
+    let cx = Context::new();
 
     // Load the program and trace from files
     let mut prog = Vec::new();
@@ -300,22 +376,21 @@ fn main() -> io::Result<()> {
     }
 
     // Generate IR code to check the trace
-    let mut ok = Vec::new();
 
-    ok.append(&mut check_first(&b, &prog, trace.first().unwrap()));
+    check_first(&cx, &b, &prog, trace.first().unwrap());
 
     for (i, (s1, s2)) in trace.iter().zip(trace.iter().skip(1)).enumerate() {
         let port = &mem_ports[i];
-        ok.append(&mut check_step(&b, i as u32, &prog, &[port.clone()], s1, s2));
+        check_step(&cx, &b, i as u32, &prog, &[port.clone()], s1, s2);
     }
 
-    ok.append(&mut check_last(&b, &prog, trace.last().unwrap()));
+    check_last(&cx, &b, &prog, trace.last().unwrap());
 
     // Check the memory ports
     for (i, port) in mem_ports.iter().enumerate() {
         // Currently, ports have a 1-to-1 mapping to steps.  We check that either the port is used
         // in its corresponding cycle, or it isn't used at all.
-        ok.push(b.or(
+        cx.assert(b.or(
             b.eq(port.cycle, b.lit(i as u32)),
             b.eq(port.cycle, b.lit(!0_u32)),
         ));
@@ -329,36 +404,49 @@ fn main() -> io::Result<()> {
             b.and(b.eq(x.addr, y.addr), b.lt(x.cycle, y.cycle)),
         )
     });
-    ok.append(&mut check_first_mem(&b, &sorted_mem[0]));
+    check_first_mem(&cx, &b, &sorted_mem[0]);
     for (port1, port2) in sorted_mem.iter().zip(sorted_mem.iter().skip(1)) {
-        ok.append(&mut check_mem(&b, port1, port2));
+        check_mem(&cx, &b, port1, port2);
     }
 
     // Lower IR code
     let c = b.finish();
 
-    let ok = ok.into_iter().map(|tw| tw.repr).collect::<Vec<_>>();
+    let (asserts, bugs) = cx.finish();
+    let num_asserts = asserts.len();
+    let flags = asserts.into_iter().chain(bugs.into_iter())
+        .map(|tw| tw.repr).collect::<Vec<_>>();
+
     // TODO: need a better way to handle passes that must be run to fixpoint
-    let ok = run_pass(&c, ok, lower::gadget::decompose_all_gadgets);
-    let ok = run_pass(&c, ok, lower::gadget::decompose_all_gadgets);
-    let ok = run_pass(&c, ok, lower::bundle::unbundle_mux);
-    let ok = run_pass(&c, ok, lower::bundle::simplify);
-    let ok = run_pass(&c, ok, lower::int::mod_to_div);
-    let ok = run_pass(&c, ok, lower::int::non_constant_shift);
-    let ok = run_pass(&c, ok, lower::int::extend_to_64);
-    let ok = run_pass(&c, ok, lower::int::int_to_uint);
-    let ok = run_pass(&c, ok, lower::int::reduce_lit_32);
-    let ok = run_pass(&c, ok, lower::int::mux);
-    let ok = run_pass(&c, ok, lower::int::compare_to_zero);
-    let ok = run_pass(&c, ok, lower::bool_::mux);
-    let ok = run_pass(&c, ok, lower::bool_::compare_to_logic);
-    let ok = run_pass(&c, ok, lower::bool_::not_to_xor);
+    let flags = run_pass(&c, flags, lower::gadget::decompose_all_gadgets);
+    let flags = run_pass(&c, flags, lower::gadget::decompose_all_gadgets);
+    let flags = run_pass(&c, flags, lower::bundle::unbundle_mux);
+    let flags = run_pass(&c, flags, lower::bundle::simplify);
+    let flags = run_pass(&c, flags, lower::int::mod_to_div);
+    let flags = run_pass(&c, flags, lower::int::non_constant_shift);
+    let flags = run_pass(&c, flags, lower::int::extend_to_64);
+    let flags = run_pass(&c, flags, lower::int::int_to_uint);
+    let flags = run_pass(&c, flags, lower::int::reduce_lit_32);
+    let flags = run_pass(&c, flags, lower::int::mux);
+    let flags = run_pass(&c, flags, lower::int::compare_to_zero);
+    let flags = run_pass(&c, flags, lower::bool_::mux);
+    let flags = run_pass(&c, flags, lower::bool_::compare_to_logic);
+    let flags = run_pass(&c, flags, lower::bool_::not_to_xor);
 
     // Generate SCALE
     let mut backend = back::scale::Backend::new();
-    backend.print_str("results: ");
-    for w in ok {
-        let sbit = backend.wire(w);
+
+    backend.print_str("asserts: ");
+    for w in flags.iter().take(num_asserts) {
+        let sbit = backend.wire(w.clone());
+        let bit = backend.reveal(sbit);
+        backend.print(bit);
+    }
+    backend.print_str("\n");
+
+    backend.print_str("bugs: ");
+    for w in flags.iter().skip(num_asserts) {
+        let sbit = backend.wire(w.clone());
         let bit = backend.reveal(sbit);
         backend.print(bit);
     }
