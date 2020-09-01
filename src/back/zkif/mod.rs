95c9e6b6212d83d58784db39bf9cd6f2a4037fca
aurel <aurel@qed-it.com>
2020-09-01 10:46:06 +0200

    zkif-bellman: remove unused code

--- src/back/zkif/mod.rs
+++ src/back/zkif/mod.rs
@@ -1,3 +1,9 @@
+use zkinterface::{
+    statement::{StatementBuilder, Store, FileStore},
+    Result,
+    CircuitOwned, VariablesOwned, KeyValueOwned,
+};
+
 pub mod backend;
 mod zkif_cs;
 mod representer;
@@ -6,18 +12,4 @@ mod bit_width;
 mod int;
 mod uint32;
 
-//mod gadget_specs;
-//mod prototype_backend;
-//mod mem;
 //mod debug;
-
-#[cfg(feature = "libsnark")]
-mod gadgetlib;
-#[cfg(feature = "libsnark")]
-mod machine;
-
-use zkinterface::{
-    statement::{StatementBuilder, Store, FileStore},
-    Result,
-    CircuitOwned, VariablesOwned, KeyValueOwned,
-};
