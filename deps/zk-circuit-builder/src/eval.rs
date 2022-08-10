464409b25314408ba329e790920cfa5cd0b0bdb6
James Parker <james@galois.com>
2022-08-10 17:44:34 -0400

    Fix types

--- deps/zk-circuit-builder/src/eval.rs
+++ deps/zk-circuit-builder/src/eval.rs
@@ -263,9 +263,10 @@ pub fn eval_unop_integer<'a>(
     c: &CircuitBase<'a>,
     op: UnOp,
     a_bits: Bits<'a>,
+    a_ty: Ty<'a>,
     ty: Ty<'a>,
 ) -> Bits<'a> {
-    let a_val = a_bits.to_bigint(ty);
+    let a_val = a_bits.to_bigint(a_ty);
     let val = match op {
         UnOp::Not => !a_val,
         UnOp::Neg => -a_val,
@@ -303,11 +304,13 @@ pub fn eval_binop_integer<'a>(
     c: &CircuitBase<'a>,
     op: BinOp,
     a_bits: Bits<'a>,
+    a_ty: Ty<'a>,
     b_bits: Bits<'a>,
+    b_ty: Ty<'a>,
     ty: Ty<'a>,
 ) -> Bits<'a> {
-    let a_val = a_bits.to_bigint(ty);
-    let b_val = b_bits.to_bigint(ty);
+    let a_val = a_bits.to_bigint(a_ty);
+    let b_val = b_bits.to_bigint(b_ty);
     let val = match op {
         BinOp::Add => a_val + b_val,
         BinOp::Sub => a_val - b_val,
@@ -359,11 +362,13 @@ pub fn eval_cmp_integer<'a>(
     c: &CircuitBase<'a>,
     op: CmpOp,
     a_bits: Bits<'a>,
+    a_ty: Ty<'a>,
     b_bits: Bits<'a>,
+    b_ty: Ty<'a>,
     ty: Ty<'a>,
 ) -> Bits<'a> {
-    let a_val = a_bits.to_bigint(ty);
-    let b_val = b_bits.to_bigint(ty);
+    let a_val = a_bits.to_bigint(a_ty);
+    let b_val = b_bits.to_bigint(b_ty);
     let val: bool = match op {
         CmpOp::Eq => a_val == b_val,
         CmpOp::Ne => a_val != b_val,
@@ -425,7 +430,7 @@ pub fn eval_gate<'a>(
         GateKind::Unary(op, a) => {
             let (a_bits, a_sec) = get_value(a)?;
             let result_bits = if a.ty.is_integer() {
-                eval_unop_integer(c, op, a_bits, a.ty)
+                eval_unop_integer(c, op, a_bits, a.ty, ty)
             } else if let Some(f) = a.ty.get_galois_field() {
                 eval_unop_galois_field(c, op, a_bits, f)
             } else {
@@ -438,7 +443,7 @@ pub fn eval_gate<'a>(
             let (a_bits, a_sec) = get_value(a)?;
             let (b_bits, b_sec) = get_value(b)?;
             let result_bits = if a.ty.is_integer() {
-                eval_binop_integer(c, op, a_bits, b_bits, ty)
+                eval_binop_integer(c, op, a_bits, a.ty, b_bits, b.ty, ty)
             } else if let Some(f) = a.ty.get_galois_field() {
                 eval_binop_galois_field(c, op, a_bits, b_bits, f)
             } else {
@@ -466,7 +471,7 @@ pub fn eval_gate<'a>(
             let (a_bits, a_sec) = get_value(a)?;
             let (b_bits, b_sec) = get_value(b)?;
             let result_bits = if a.ty.is_integer() {
-                eval_cmp_integer(c, op, a_bits, b_bits, ty)
+                eval_cmp_integer(c, op, a_bits, a.ty, b_bits, b.ty, ty)
             } else if let Some(f) = a.ty.get_galois_field() {
                 eval_cmp_galois_field(c, op, a_bits, b_bits, f)
             } else {
