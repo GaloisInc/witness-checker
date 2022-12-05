478bdd44ebc4829dd9c0fb2bf92c87344523e6d7
Stuart Pernsteiner <spernsteiner@galois.com>
2022-12-05 14:22:17 -0800

    add new WideMul2 gadget, returning a single large Int

--- deps/zk-circuit-builder/src/gadget/arith.rs
+++ deps/zk-circuit-builder/src/gadget/arith.rs
@@ -1,7 +1,7 @@
 use num_bigint::{BigInt, BigUint};
 use crate::eval::{Value, EvalResult};
 use crate::ir::circuit::{
-    CircuitExt, CircuitBase, DynCircuitRef, Wire, Ty, TyKind, GadgetKind, GadgetKindRef,
+    CircuitExt, CircuitBase, DynCircuitRef, Wire, Ty, TyKind, IntSize, GadgetKind, GadgetKindRef,
 };
 use crate::ir::typed::{Builder, AsBuilder, Repr, TWire};
 
@@ -213,6 +213,113 @@ impl<'a> GadgetKind<'a> for WideMul {
 }
 
 
+/// Perform double-word multiplication (for example, a `32 x 32 -> 64` bit multiply).
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+pub struct WideMul2;
+impl_gadget_kind_support!(WideMul2);
+
+impl<'a> GadgetKind<'a> for WideMul2 {
+    fn transfer<'b>(&self, c: &CircuitBase<'b>) -> GadgetKindRef<'b> {
+        c.intern_gadget_kind(self.clone())
+    }
+
+    fn typecheck(&self, c: &CircuitBase<'a>, arg_tys: &[Ty<'a>]) -> Ty<'a> {
+        assert!(arg_tys.len() == 2, "expected exactly 2 arguments");
+        let (a_sz, a_sign) = match *arg_tys[0] {
+            TyKind::Uint(sz) => (sz, false),
+            TyKind::Int(sz) => (sz, true),
+            _ => panic!("expected Uint or Int, but got {:?}", arg_tys[0]),
+        };
+        let (b_sz, b_sign) = match *arg_tys[1] {
+            TyKind::Uint(sz) => (sz, false),
+            TyKind::Int(sz) => (sz, true),
+            _ => panic!("expected Uint or Int, but got {:?}", arg_tys[1]),
+        };
+        assert_eq!(a_sz, b_sz, "WideMul inputs must have the same width");
+
+        if a_sign || b_sign {
+            c.ty(TyKind::Int(IntSize(a_sz.bits() * 2)))
+        } else {
+            c.ty(TyKind::Uint(IntSize(a_sz.bits() * 2)))
+        }
+    }
+
+    fn decompose(&self, c: DynCircuitRef<'a, '_>, args: &[Wire<'a>]) -> Wire<'a> {
+        let (x_sz, x_sign) = match *args[0].ty {
+            TyKind::Uint(sz) => (sz, false),
+            TyKind::Int(sz) => (sz, true),
+            _ => panic!("expected Uint or Int, but got {:?}", args[0].ty),
+        };
+        let (y_sz, y_sign) = match *args[1].ty {
+            TyKind::Uint(sz) => (sz, false),
+            TyKind::Int(sz) => (sz, true),
+            _ => panic!("expected Uint or Int, but got {:?}", args[1].ty),
+        };
+        debug_assert_eq!(x_sz, y_sz, "WideMul inputs must have the same width");
+        let sz = x_sz;
+        let sign = x_sign || y_sign;
+
+        let sz_round_up = IntSize((sz.bits() + 1) & !1);
+        let sz_double = IntSize(sz.bits() * 2);
+
+        let uty = c.ty(TyKind::Uint(sz_round_up));
+        let ity = c.ty(TyKind::Int(sz_round_up));
+        let out_ty = if sign {
+            c.ty(TyKind::Int(sz_double))
+        } else {
+            c.ty(TyKind::Uint(sz_double))
+        };
+
+        let n = sz.bits();
+        let m = (sz.bits() + 1) / 2;
+        assert!(n <= 255);
+
+        let x = args[0];
+        let y = args[1];
+
+        let x0 = c.and(c.cast(x, uty), c.lit(uty, (BigUint::from(1_u8) << m) - 1_u8));
+        // Cast after shift so the sign extension takes effect.
+        let x1 = c.cast(c.shr(x, c.lit(Ty::uint(8), m)), uty);
+        let y0 = c.and(c.cast(y, uty), c.lit(uty, (BigUint::from(1_u8) << m) - 1_u8));
+        let y1 = c.cast(c.shr(y, c.lit(Ty::uint(8), m)), uty);
+
+        let z2 = c.mul(x1, y1);
+        let z1 = {
+            // In each product, sign-extend if the high (1) operand of the input is signed.
+            let x0y1 = if y_sign {
+                c.cast(c.cast(c.mul(x0, y1), ity), out_ty)
+            } else {
+                c.cast(c.mul(x0, y1), out_ty)
+            };
+            let x1y0 = if x_sign {
+                c.cast(c.cast(c.mul(x1, y0), ity), out_ty)
+            } else {
+                c.cast(c.mul(x1, y0), out_ty)
+            };
+            c.add(x0y1, x1y0)
+        };
+        let z0 = c.mul(x0, y0);
+
+        let out = c.add(c.add(
+                c.cast(z0, out_ty),
+                c.shl(c.cast(z1, out_ty), c.lit(Ty::uint(8), m)),
+            ),
+            c.shl(c.cast(z2, out_ty), c.lit(Ty::uint(8), 2 * m)),
+        );
+
+        out
+    }
+
+    fn eval(&self, arg_tys: &[Ty<'a>], args: &[EvalResult<'a>]) -> EvalResult<'a> {
+        let sz = arg_tys[0].integer_size();
+        let a = args[0].as_ref()?.as_single().unwrap();
+        let b = args[1].as_ref()?.as_single().unwrap();
+        let product = a * b;
+        Ok(Value::SingleInteger(product))
+    }
+}
+
+
 pub trait BuilderExt<'a>: AsBuilder<'a> {
     fn add_with_overflow<A: AddWithOverflowTrait<'a, B>, B: Repr<'a>>(
         &self,
@@ -341,5 +448,75 @@ impl<'a> WideMulTrait<'a> for i64 {
         let g = c.intern_gadget_kind(WideMul);
         let out = c.gadget(g, &[a, b]);
         (TWire::new(c.extract(out, 0)), TWire::new(c.extract(out, 1)))
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::eval;
+    use crate::ir::circuit::{Circuit, FilterNil, Arenas};
+    use crate::lower::gadget::DecomposeGadgets;
+    use super::*;
+
+    fn wide_mul2_common(a_ty: TyKind<'static>, b_ty: TyKind<'static>) {
+        // We use two separate, unrelated lifetimes for the two circuit arenas to ensure that wires
+        // for the two circuits can't be mixed up.
+        fn inner<'a, 'b>(
+            arenas1: &'a Arenas,
+            arenas2: &'b Arenas,
+            a_ty: TyKind<'static>,
+            b_ty: TyKind<'static>,
+        ) {
+            let c1 = Circuit::new(arenas1, true, FilterNil);
+            let c2 = Circuit::new(arenas2, true, DecomposeGadgets::new(FilterNil, |_| true));
+
+            let gk1 = c1.intern_gadget_kind(WideMul2);
+            let gk2 = c2.intern_gadget_kind(WideMul2);
+
+            for a_val in 0 .. 16 {
+                let a1 = c1.lit(c1.ty(a_ty), a_val);
+                let a2 = c2.lit(c2.ty(a_ty), a_val);
+                for b_val in 0 .. 16 {
+                    let b1 = c1.lit(c1.ty(b_ty), b_val);
+                    let b2 = c2.lit(c2.ty(b_ty), b_val);
+
+                    let out1 = c1.gadget(gk1, &[a1, b1]);
+                    let out2 = c2.gadget(gk2, &[a2, b2]);
+
+                    let out1_val = eval::eval_wire_public(&c1, out1);
+                    let out2_val = eval::eval_wire_public(&c2, out2);
+                    assert_eq!(out1_val, out2_val, "on inputs {}, {}", a_val, b_val);
+                }
+            }
+        }
+
+        let arenas = Arenas::new();
+        inner(&arenas, &arenas, a_ty, b_ty);
+    }
+
+    #[test]
+    fn wide_mul2_unsigned() {
+        wide_mul2_common(TyKind::Uint(IntSize(4)), TyKind::Uint(IntSize(4)));
+    }
+
+    #[test]
+    fn wide_mul2_signed() {
+        wide_mul2_common(TyKind::Int(IntSize(4)), TyKind::Int(IntSize(4)));
+    }
+
+    #[test]
+    fn wide_mul2_mixed() {
+        wide_mul2_common(TyKind::Int(IntSize(4)), TyKind::Uint(IntSize(4)));
+        wide_mul2_common(TyKind::Uint(IntSize(4)), TyKind::Int(IntSize(4)));
+    }
+
+
+    #[test]
+    fn wide_mul2_odd() {
+        wide_mul2_common(TyKind::Uint(IntSize(3)), TyKind::Uint(IntSize(3)));
+        wide_mul2_common(TyKind::Int(IntSize(3)), TyKind::Int(IntSize(3)));
+        wide_mul2_common(TyKind::Int(IntSize(3)), TyKind::Uint(IntSize(3)));
+        wide_mul2_common(TyKind::Uint(IntSize(3)), TyKind::Int(IntSize(3)));
     }
 }
