008a2758ba44c09364ae5b12a6a7e8c4c6218a7a
Stuart Pernsteiner <spernsteiner@galois.com>
2023-04-07 09:12:47 -0700

    micro_ram::witness: add routing network info to SegmentWitness

--- src/micro_ram/witness.rs
+++ src/micro_ram/witness.rs
@@ -6,6 +6,16 @@ use crate::micro_ram::types::{MultiExec, ExecBody, Segment, Advice};
 pub struct SegmentWitness {
     pub advice: Vec<u64>,
     pub stutter: Vec<bool>,
+
+    /// Index of the previous segment in the trace, if any.
+    pub pred: Option<usize>,
+    /// Index of the next segment in the trace, if any.
+    pub succ: Option<usize>,
+    /// Whether the connection from the previous segment to the current one went through the
+    /// routing network.
+    pub from_net: bool,
+    /// Whether the connection from this segment to the next one went through the routing network.
+    pub to_net: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -36,9 +46,11 @@ impl ExecWitness {
         };
 
         let mut i = 0;
+        let mut prev_seg_idx: Option<usize> = None;
         for tc in &e.trace {
-            let seg = &e.segments[tc.segment];
-            let seg_w = &mut w.segments[tc.segment];
+            let seg_idx = tc.segment;
+            let seg = &e.segments[seg_idx];
+            let seg_w = &mut w.segments[seg_idx];
             for j in 0 .. seg.len {
                 if let Some(advs) = e.advice.get(&(i + 1)) {
                     for adv in advs {
@@ -51,6 +63,19 @@ impl ExecWitness {
                 }
                 i += 1;
             }
+
+            if let Some(prev_seg_idx) = prev_seg_idx {
+                let prev_seg = &e.segments[prev_seg_idx];
+                let direct_connect = prev_seg.successors.contains(&seg_idx);
+                seg_w.pred = Some(prev_seg_idx);
+                seg_w.from_net = !direct_connect;
+
+                let prev_seg_w = &mut w.segments[prev_seg_idx];
+                prev_seg_w.succ = Some(seg_idx);
+                prev_seg_w.to_net = !direct_connect;
+            }
+
+            prev_seg_idx = Some(seg_idx);
         }
 
         w
@@ -62,6 +87,11 @@ impl SegmentWitness {
         SegmentWitness {
             advice: vec![0; s.len],
             stutter: vec![false; s.len],
+
+            pred: None,
+            succ: None,
+            from_net: false,
+            to_net: false,
         }
     }
 }
