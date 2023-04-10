841645949a463ab9b37c7e424335bcd31665ac75
Stuart Pernsteiner <spernsteiner@galois.com>
2023-04-10 16:09:02 -0700

    ir::circuit: let functions have a different secret type

--- deps/zk-circuit-builder/src/ir/circuit.rs
+++ deps/zk-circuit-builder/src/ir/circuit.rs
@@ -17,6 +17,7 @@ use std::collections::{HashMap, HashSet};
 use std::convert::TryFrom;
 use std::fmt;
 use std::hash::{Hash, Hasher};
+use std::iter;
 use std::marker::PhantomData;
 use std::mem::{self, ManuallyDrop};
 use std::ptr;
@@ -57,7 +58,7 @@ pub struct CircuitBase<'a> {
     current_label: Cell<&'a str>,
     is_prover: bool,
     functions: RefCell<Vec<Function<'a>>>,
-    witness_type: TypeId,
+    witness_type: Cell<TypeId>,
 }
 
 struct FunctionScope<'a> {
@@ -79,7 +80,7 @@ impl<'a> CircuitBase<'a> {
             current_label: Cell::new(""),
             is_prover,
             functions: RefCell::new(Vec::new()),
-            witness_type: TypeId::of::<W>(),
+            witness_type: Cell::new(TypeId::of::<W>()),
         };
         c.preload_common();
         c
@@ -118,7 +119,7 @@ impl<'a> CircuitBase<'a> {
 
     pub fn with_witness_type<W: 'static>(&self) -> Option<&CircuitBaseWithWitnessType<'a, W>> {
         unsafe {
-            if self.witness_type == TypeId::of::<W>() {
+            if self.witness_type.get() == TypeId::of::<W>() {
                 Some(self.with_witness_type_unchecked())
             } else {
                 None
@@ -334,17 +335,57 @@ impl<'a> CircuitBase<'a> {
         }
     }
 
+    fn alloc_secret_project_fn<W: 'static, T: 'static, F>(&self, f: F) -> SecretProjectFn<'a>
+    where
+        F: for<'b, 'w> Fn(&CircuitBase<'b>, &'w W, &[Bits<'b>]) -> CowBox<'w, T>,
+        F: Sized + Copy + 'static,
+    {
+        fn erase<W, T: 'static>(
+            f: impl for<'b, 'w> Fn(
+                &CircuitBase<'b>,
+                &'w W,
+                &[Bits<'b>],
+            ) -> CowBox<'w, T> + Copy + 'static,
+        ) -> impl for<'b> Fn(
+            &CircuitBase<'b>,
+            *const (),
+            &[Bits<'b>],
+        ) -> CowBox<'static, dyn Any> + Copy + 'static {
+            move |c, witness, dep_vals| {
+                unsafe {
+                    let witness = &*(witness as *const W);
+                    let out = f(c, witness, dep_vals);
+                    // Cast away the lifetime
+                    match out {
+                        CowBox::Owned(b) => {
+                            CowBox::Owned(b as Box<dyn Any>)
+                        },
+                        CowBox::Borrowed(r) => {
+                            let r: *const dyn Any = r;
+                            CowBox::Borrowed(&*r)
+                        },
+                    }
+                }
+            }
+        }
+
+        unsafe {
+            let r = self.arena().alloc(erase::<W, T>(f));
+            SecretProjectFn::new(IsCopy::new_ref(r) as _)
+        }
+    }
+
     fn alloc_lazy_secret<W, F>(&self, ty: Ty<'a>, deps: &'a [Wire<'a>], init: F) -> Secret<'a>
     where
         W: 'static,
         F: for<'b> Fn(&CircuitBase<'b>, &W, &[Bits<'b>]) -> Bits<'b>,
         F: Sized + Copy + 'static,
     {
-        // `W` must match this circuit's witness type.  As a special case, we also allow `()`,
-        // which essentially means that the `&W` input will be ignored.  We don't allow other ZSTs
-        // here because some ZSTs are used as markers, where having a value of that type means that
-        // some property holds.
-        assert!(TypeId::of::<W>() == self.witness_type ||
+        // `W` must match this circuit's witness type.  As a special case, we also allow `()`, which
+        // essentially means that the `&W` input will be ignored.  We don't allow other ZSTs here
+        // because some ZSTs are used as markers, where having a value of that type means that some
+        // property holds.
+        assert!(TypeId::of::<W>() == self.witness_type.get() ||
             TypeId::of::<W>() == TypeId::of::<()>());
         let sd = if self.is_prover {
             let init = self.alloc_secret_init_fn::<W, F>(init);
@@ -410,7 +451,7 @@ impl<'a> CircuitBase<'a> {
             ref current_label,
             is_prover,
             ref functions,
-            witness_type,
+            ref witness_type,
         } = *self;
 
         let old_arenas = Box::new(arenas.take());
@@ -429,7 +470,7 @@ impl<'a> CircuitBase<'a> {
             current_label: Cell::new(current_label.replace("")),
             is_prover,
             functions: RefCell::new(functions.take()),
-            witness_type,
+            witness_type: Cell::new(witness_type.get()),
         };
 
         self.preload_common();
@@ -550,6 +591,7 @@ impl<'a, F: CircuitFilter<'a> + ?Sized> Circuit<'a, F> {
             arg_tys: self.ty_list(arg_tys),
             secret_inputs: self.as_base().arena().alloc_slice_copy(&new_scope.secrets),
             result_wire,
+            witness_type: TypeId::of::<()>(),
         }));
         self.as_base().functions.borrow_mut().push(func);
         (func, extra)
@@ -562,6 +604,55 @@ impl<'a, F: CircuitFilter<'a> + ?Sized> Circuit<'a, F> {
             arg_tys,
             |c, args| (f(c, args), ()),
         );
+        func
+    }
+
+    /// Define a function.  The closure receives a list of argument wires (of types `arg_tys`), and
+    /// returns a wire representing the output of the function.
+    ///
+    /// Within the function, lazy secrets use a witness type of `W2` rather than the witness type
+    /// of the enclosing circuit.  Calls to the function will need to provide a `SecretProjectFn`
+    /// to convert from the outer witness type to the inner one.
+    ///
+    /// Concretely, the `Circuit` passed to the callback is `self` and uses the same lifetime `'a`,
+    /// but we hide this fact from the caller so that `rustc` will report an error if wires from
+    /// outside the function are used inside the callback or vice versa.
+    //
+    // This function is defined on the concrete `Circuit` type instead of the generic `CircuitExt`
+    // trait because this lets it provide a more precise circuit type to the callback, which
+    // simplifies its usage with `typed::Builder`.  However, it should be easy to copy into
+    // `CircuitExt` if it's needed in the future.
+    pub fn define_function2<W2: 'static, F2>(
+        &self,
+        name: &str,
+        arg_tys: &[Ty<'a>],
+        f: F2,
+    ) -> Function<'a>
+    where F2: for<'b> FnOnce(&Circuit<'b, F>, &[Wire<'b>]) -> Wire<'b> {
+        let inner_witness_type = TypeId::of::<W2>();
+        let old_witness_type = self.as_base().witness_type.replace(inner_witness_type);
+
+        let function_scope = &self.base.function_scope;
+        let old_scope = mem::replace(&mut *function_scope.borrow_mut(), Some(FunctionScope {
+            secrets: Vec::new(),
+        }));
+
+        let arg_wires = arg_tys.iter().enumerate()
+            .map(|(i, &ty)| self.gate(GateKind::Argument(i, ty)))
+            .collect::<Vec<_>>();
+        let result_wire = f(self, &arg_wires);
+
+        let new_scope = mem::replace(&mut *function_scope.borrow_mut(), old_scope).unwrap();
+        let func = Function(self.as_base().arena().alloc(FunctionDef {
+            name: self.as_base().intern_str(name),
+            arg_tys: self.ty_list(arg_tys),
+            secret_inputs: self.as_base().arena().alloc_slice_copy(&new_scope.secrets),
+            result_wire,
+            witness_type: inner_witness_type,
+        }));
+        self.as_base().functions.borrow_mut().push(func);
+
+        self.as_base().witness_type.set(old_witness_type);
         func
     }
 }
@@ -1079,6 +1170,35 @@ pub trait CircuitExt<'a>: CircuitTrait<'a> {
             func,
             args,
             secret_args: secrets,
+            project_witness: None,
+            project_deps: &[],
+            outer_witness_type: self.as_base().witness_type.get(),
+        });
+        self.gate(GateKind::Call(call))
+    }
+
+    fn call2<W, W2, F>(
+        &self,
+        func: Function<'a>,
+        args: &'a [Wire<'a>],
+        project_deps: &'a [Wire<'a>],
+        project_witness: F,
+    ) -> Wire<'a>
+    where
+        W: 'static,
+        W2: 'static,
+        F: for<'b, 's> Fn(&CircuitBase<'b>, &'s W, &[Bits<'b>]) -> CowBox<'s, W2>,
+        F: Sized + Copy + 'static,
+    {
+        debug_assert_eq!(TypeId::of::<W>(), self.as_base().witness_type.get());
+        let project_witness = self.as_base().alloc_secret_project_fn(project_witness);
+        let call = self.as_base().alloc_call(CallData {
+            func,
+            args,
+            secret_args: &[],
+            project_witness: Some(project_witness),
+            project_deps,
+            outer_witness_type: self.as_base().witness_type.get(),
         });
         self.gate(GateKind::Call(call))
     }
@@ -1384,6 +1504,7 @@ pub struct WireDeps<'a> {
 enum WireDepsInner<'a> {
     Small(Range<u8>, [Option<Wire<'a>>; 3]),
     Large(slice::Iter<'a, Wire<'a>>),
+    Large2(iter::Chain<slice::Iter<'a, Wire<'a>>, slice::Iter<'a, Wire<'a>>>),
 }
 
 impl<'a> WireDeps<'a> {
@@ -1414,6 +1535,12 @@ impl<'a> WireDeps<'a> {
             inner: WireDepsInner::Large(ws.iter()),
         }
     }
+
+    fn many2(ws1: &'a [Wire<'a>], ws2: &'a [Wire<'a>]) -> WireDeps<'a> {
+        WireDeps {
+            inner: WireDepsInner::Large2(ws1.iter().chain(ws2.iter())),
+        }
+    }
 }
 
 impl<'a> Iterator for WireDepsInner<'a> {
@@ -1425,6 +1552,7 @@ impl<'a> Iterator for WireDepsInner<'a> {
                 arr.get(i).and_then(|&x| x)
             },
             WireDepsInner::Large(ref mut it) => it.next().cloned(),
+            WireDepsInner::Large2(ref mut it) => it.next().cloned(),
         }
     }
 }
@@ -1437,6 +1565,7 @@ impl<'a> DoubleEndedIterator for WireDepsInner<'a> {
                 arr.get(i).and_then(|&x| x)
             },
             WireDepsInner::Large(ref mut it) => it.next_back().cloned(),
+            WireDepsInner::Large2(ref mut it) => it.next_back().cloned(),
         }
     }
 }
@@ -1484,6 +1613,7 @@ pub fn wire_and_secret_deps<'a>(w: Wire<'a>) -> WireDeps<'a> {
 pub fn gate_and_secret_deps<'a>(gk: GateKind<'a>) -> WireDeps<'a> {
     match gk {
         GateKind::Secret(s) => WireDeps::many(s.deps),
+        GateKind::Call(c) => WireDeps::many2(c.args, c.project_deps),
         _ => gate_deps(gk),
     }
 }
@@ -2475,6 +2605,13 @@ impl<'a, T: ?Sized> Deref for ArenaDyn<'a, T> {
 type SecretInitFn<'a> = ArenaDyn<'a,
     dyn for<'b> Fn(&CircuitBase<'b>, *const (), &[Bits<'b>]) -> Bits<'b> + 'static>;
 
+type SecretProjectFn<'a> = ArenaDyn<'a,
+    dyn for<'b> Fn(
+        &CircuitBase<'b>,
+        *const (),
+        &[Bits<'b>],
+    ) -> CowBox<'static, dyn Any> + 'static>;
+
 #[derive(Clone, Debug)]
 pub struct SecretData<'a> {
     pub ty: Ty<'a>,
@@ -2684,6 +2821,10 @@ pub struct CallData<'a> {
     pub func: Function<'a>,
     pub args: &'a [Wire<'a>],
     pub secret_args: &'a [(SecretInputId, Secret<'a>)],
+
+    project_witness: Option<SecretProjectFn<'a>>,
+    pub project_deps: &'a [Wire<'a>],
+    outer_witness_type: TypeId,
 }
 
 impl<'a, 'b> Migrate<'a, 'b> for CallData<'a> {
@@ -2693,10 +2834,15 @@ impl<'a, 'b> Migrate<'a, 'b> for CallData<'a> {
         let args = v.new_circuit().intern_wire_list(&args);
         let secret_args = self.secret_args.iter().map(|&w| v.visit(w)).collect::<Vec<_>>();
         let secret_args = v.new_circuit().arena().alloc_slice_copy(&secret_args);
+        let project_deps = self.project_deps.iter().map(|&w| v.visit(w)).collect::<Vec<_>>();
+        let project_deps = v.new_circuit().arena().alloc_slice_copy(&project_deps);
         CallData {
             func: v.visit(self.func),
             args,
             secret_args,
+            project_witness: v.visit(self.project_witness),
+            project_deps,
+            outer_witness_type: self.outer_witness_type,
         }
     }
 }
@@ -2707,6 +2853,28 @@ impl<'a, 'b> Migrate<'a, 'b> for Call<'a> {
     fn migrate<V: migrate::Visitor<'a, 'b> + ?Sized>(self, v: &mut V) -> Call<'b> {
         let new = v.visit((*self).clone());
         v.new_circuit().alloc_call(new)
+    }
+}
+
+impl<'a> CallData<'a> {
+    pub fn has_project_witness(&self) -> bool {
+        self.project_witness.is_some()
+    }
+
+    pub fn project_witness<'w>(
+        &self,
+        c: &CircuitBase<'a>,
+        witness: &'w dyn Any,
+        dep_vals: &[Bits<'a>],
+    ) -> CowBox<'w, dyn Any> {
+        assert!(self.outer_witness_type == witness.type_id() ||
+            self.outer_witness_type == TypeId::of::<()>());
+        let project_witness = self.project_witness.unwrap();
+        let new_witness: CowBox<'w, dyn Any> = unsafe {
+            project_witness(c, witness as *const dyn Any as *const (), dep_vals)
+        };
+        debug_assert_eq!((*new_witness).type_id(), self.func.witness_type);
+        new_witness
     }
 }
 
@@ -2895,6 +3063,7 @@ pub struct FunctionDef<'a> {
     /// `Argument` gates.  For functions that need to return multiple values, this wire can have a
     /// `Bundle` type.
     pub result_wire: Wire<'a>,
+    witness_type: TypeId,
 }
 
 impl<'a, 'b> Migrate<'a, 'b> for FunctionDef<'a> {
@@ -2908,6 +3077,7 @@ impl<'a, 'b> Migrate<'a, 'b> for FunctionDef<'a> {
             arg_tys: v.new_circuit().intern_ty_list(&arg_tys),
             secret_inputs: v.new_circuit().arena().alloc_slice_copy(&secret_inputs),
             result_wire: v.visit(self.result_wire),
+            witness_type: self.witness_type,
         }
     }
 }
--- deps/zk-circuit-builder/tests/function_gate.rs
+++ deps/zk-circuit-builder/tests/function_gate.rs
@@ -1,8 +1,9 @@
 use std::convert::TryInto;
 use zk_circuit_builder::eval;
 use zk_circuit_builder::ir::circuit::{
-    Arenas, Circuit, CircuitTrait, CircuitExt, Wire, Ty, FilterNil, GateValue,
+    Arenas, Circuit, CircuitTrait, CircuitExt, Wire, Ty, FilterNil, GateValue, AsBits, IntSize,
 };
+use zk_circuit_builder::util::CowBox;
 
 macro_rules! init_circuit {
     ($c:ident) => {
@@ -90,4 +91,55 @@ fn function_gate_secret() {
         eval::Value::SingleInteger(26_i32.into()),
     );
     assert!(eval::eval_wire_public(c.as_base(), result2).is_none());
+}
+
+#[test]
+fn function_gate_lazy_secret() {
+    init_circuit!(c);
+
+    let ty_i32 = Ty::int(32);
+    let func = c.define_function2::<i32, _>("f", &[ty_i32], |c, args| {
+        let &[x]: &[Wire; 1] = args.try_into().unwrap();
+        let y = c.secret_lazy(ty_i32, |c, &y: &i32| y.as_bits(c, IntSize(32)));
+        c.add(x, y)
+    });
+
+    // Call, with projection function returning `&'static i32`
+    let args1 = [
+        c.lit(ty_i32, 1),
+    ];
+    let result1 = c.call2(func, &args1, &[], |_, &(), _| CowBox::from(&2));
+    assert_eq!(
+        eval::eval_wire_secret(c.as_base(), result1).unwrap(),
+        eval::Value::SingleInteger(3_i32.into()),
+    );
+    assert!(eval::eval_wire_public(c.as_base(), result1).is_none());
+
+    // Call, with projection function returning `Box<i32>`
+    let args2 = [
+        c.lit(ty_i32, 3),
+    ];
+    let result2 = c.call2(func, &args2, &[], |_, &(), _| CowBox::from(Box::new(4)));
+    assert_eq!(
+        eval::eval_wire_secret(c.as_base(), result2).unwrap(),
+        eval::Value::SingleInteger(7_i32.into()),
+    );
+    assert!(eval::eval_wire_public(c.as_base(), result2).is_none());
+
+    // Call, deriving the inner secret value from wire deps
+    let args3 = [
+        c.lit(ty_i32, 5),
+    ];
+    let deps3 = [
+        result2,
+    ];
+    let result3 = c.call2(func, &args3, &deps3, |_, &(), dep_vals| {
+        let x = dep_vals[0].0[0] as i32;
+        CowBox::from(Box::new(x))
+    });
+    assert_eq!(
+        eval::eval_wire_secret(c.as_base(), result3).unwrap(),
+        eval::Value::SingleInteger(12_i32.into()),
+    );
+    assert!(eval::eval_wire_public(c.as_base(), result3).is_none());
 }
