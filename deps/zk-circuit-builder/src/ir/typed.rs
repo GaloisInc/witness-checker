a4b449a497f072089b5002abaae5fecb9936b323
Marios Georgiou <marios@galois.com>
2023-11-09 16:08:17 -0500

    delete assert_perm from ir/typed.rs

--- deps/zk-circuit-builder/src/ir/typed.rs
+++ deps/zk-circuit-builder/src/ir/typed.rs
@@ -343,17 +343,6 @@ pub trait BuilderExt<'a>: Builder<'a> {
         val
     }
 
-    fn assert_perm<T>(
-        &self,
-        arr1: &[TWire<'a, T>],
-        arr2: &[TWire<'a, T>],
-    ) -> TWire<'a, bool>
-    where
-        T: Repr<'a>
-    {
-        todo!();
-    }
-
     fn index_with_default<I, T>(
         &self,
         arr: &[TWire<'a, T>],
