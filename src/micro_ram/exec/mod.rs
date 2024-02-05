308871105bca001745cd2fbb4961e82b2ea05dd9
Stuart Pernsteiner <spernsteiner@galois.com>
2024-02-05 15:46:41 -0800

    micro_ram::exec: refactor: split up `init` and `run` methods

--- src/micro_ram/exec/mod.rs
+++ src/micro_ram/exec/mod.rs
@@ -4,7 +4,7 @@ use zk_circuit_builder::eval::{self, CachingEvaluator};
 use zk_circuit_builder::hash::sha256::Sha256;
 use zk_circuit_builder::ir::circuit::{Bits, Function};
 use zk_circuit_builder::ir::migrate::{self, Migrate};
-use zk_circuit_builder::ir::migrate::handle::{MigrateContext, MigrateHandle, Rooted};
+use zk_circuit_builder::ir::migrate::handle::{MigrateContext, MigrateHandle, Rooted, Projected};
 use zk_circuit_builder::ir::typed::{Builder, BuilderExt, TWire};
 use crate::micro_ram::context::Context;
 use crate::micro_ram::fetch::Fetch;
@@ -84,7 +84,10 @@ pub fn build<'a>(
         move |w| &w.execs[exec_name],
     ));
     eb.open(mh).init(b, exec, exec_name);
-    ExecBuilder::run(&mut eb, mh, b, exec, move |w| &w.execs[exec_name]);
+    InstrTraceBuilder::run(
+        &mut eb, mh, b, exec,
+        move |w| &w.execs[exec_name],
+    );
     ExecBuilder::finish(eb, mh, b)
 }
 
@@ -133,14 +136,7 @@ impl<'a> ExecBuilder<'a, InstrTraceBuilder<'a>> {
         }
     }
 
-    fn init(
-        &mut self, b: &impl Builder<'a>, exec: &ExecBody, exec_name: &'static str) {
-        if let Some(ref out_path) = self.t.debug_segment_graph_path {
-            std::fs::write(out_path, self.t.seg_graph_builder.dump()).unwrap();
-        }
-
-        // Set up initial KnownMem
-        let mut kmem = KnownMem::with_default(b.lit(0));
+    fn init(&mut self, b: &impl Builder<'a>, exec: &ExecBody, exec_name: &'static str) {
         let mut seg_values = Vec::with_capacity(exec.init_mem.len());
         for (i, seg) in exec.init_mem.iter().enumerate() {
             let values = self.c.mem.init_segment(
@@ -150,166 +146,11 @@ impl<'a> ExecBuilder<'a, InstrTraceBuilder<'a>> {
                 self.c.equiv_segments.exec_segments(exec_name),
                 move |w| &w.execs[exec_name],
             );
-            kmem.init_segment(seg, &values);
             seg_values.push(values);
         }
-        self.t.seg_graph_builder.set_cpu_init_mem(kmem);
-        debug_assert_eq!(seg_values.len(), exec.init_mem.len());
 
-        // Populate `seg_user_map`.
-        let mut cycle = 0;
-        for (i, chunk) in exec.trace.as_instr().chunks.iter().enumerate() {
-            if let Some(c) = chunk.debug.as_ref().and_then(|d| d.cycle) {
-                cycle = c;
-            }
-
-            let old = self.t.seg_user_map.insert(chunk.segment, (i, cycle));
-            assert!(old.is_none());
-
-            cycle += chunk.states.len() as u32;
-        }
-
-        // Add extra `MemPort`s to enforce `expect_write`.
-        if let Some(addr) = self.c.expect_write {
-            // We write a 0 before execution begins, and try to read back a 1 after the program
-            // terminates.  This succeeds only if the program overwrites the 0 with a 1 during its
-            // execution.  We can't simply leave the memory uninitialized because reads from
-            // uninitialized memory are allowed (and the value produced is unconstrained).
-            self.c.mem.add_initial_write(b, addr, 0);
-            self.c.mem.add_final_read(b, addr, 1);
-        }
-
-        // Add hash check for the `commitment`.
-        if let Some(commitment) = exec.params.commitment {
-            let _g = b.scoped_label("check commitment");
-            match commitment {
-                Commitment::Sha256(expect_hash) => {
-                    let mut h = Sha256::new(b);
-
-                    for (cs, instrs) in exec.program.iter().zip(self.c.fetch.all_instrs().iter()) {
-                        if !cs.secret || cs.uncommitted {
-                            continue;
-                        }
-                        for instr in instrs {
-                            let RamInstrRepr { opcode, dest, op1, op2, imm } = instr.repr;
-                            h.push(b, opcode);
-                            h.push(b, dest);
-                            h.push(b, op1);
-                            h.push(b, op2);
-                            h.push(b, imm);
-                        }
-                    }
-
-                    for (seg, values) in exec.init_mem.iter().zip(seg_values.iter()) {
-                        if !seg.secret || seg.uncommitted {
-                            continue;
-                        }
-                        for &w in values {
-                            h.push(b, w);
-                        }
-                    }
-
-                    let actual_hash = h.finish(b);
-                    wire_assert!(
-                        cx = &self.c.cx, b, b.eq(actual_hash, b.lit(expect_hash)),
-                        "bad commitment: actual hash is {:?}, but expected {:?}",
-                        cx.eval(actual_hash), expect_hash,
-                    );
-                },
-            }
-        }
-    }
-
-    fn run(
-        this: &mut Rooted<'a, Self>,
-        mh: &mut MigrateHandle<'a>,
-        b: &impl Builder<'a>,
-        exec: &ExecBody,
-        project_witness: impl Fn(&MultiExecWitness) -> &ExecWitness + Copy + 'static,
-    ) {
-        let instr_lookup = InstrLookup::new(&exec.program);
-        for item in this.open(mh).t.seg_graph_builder.get_order() {
-            match item {
-                SegGraphItem::Segment(idx) =>
-                    this.open(mh).add_segment(b, exec, &instr_lookup, idx, project_witness),
-                SegGraphItem::Network => {
-                    unsafe { mh.erase_and_migrate(b.circuit()) };
-                    info!("seg_graph_builder.build_network");
-                    let mut seg_graph_builder = this.project(mh, |eb| &mut eb.t.seg_graph_builder);
-                    SegGraphBuilder::build_network(&mut seg_graph_builder, mh, b, project_witness);
-                    unsafe { mh.erase_and_migrate(b.circuit()) };
-                    continue;
-                },
-            }
-
-            unsafe { mh.erase_and_migrate(b.circuit()) };
-        }
-    }
-
-    fn add_segment(
-        &mut self,
-        b: &impl Builder<'a>,
-        exec: &ExecBody,
-        instr_lookup: &InstrLookup,
-        idx: usize,
-        project_witness: impl Fn(&MultiExecWitness) -> &ExecWitness + Copy + 'static,
-    ) {
-        // Build the circuit for this segment.
-        let mut segment_builder = SegmentBuilder {
-            cx: &self.c.cx,
-            b: b,
-            ev: &mut self.c.ev,
-            privilege_levels: self.c.privilege_levels,
-            calc_step_func: self.t.calc_step_func,
-            calc_step_inner_cases: &self.t.calc_step_inner_cases,
-            check_step_func: self.t.check_step_func,
-            mem: &mut self.c.mem,
-            fetch: &mut self.c.fetch,
-            params: &exec.params,
-            prog: instr_lookup,
-            check_steps: self.c.check_steps,
-        };
-
-        let seg_def = &exec.trace.as_instr().segments[idx];
-        let mut prev_state = self.t.seg_graph_builder.get_initial(b, idx).clone();
-        let prev_kmem = self.t.seg_graph_builder.take_initial_mem(idx);
-
-        let external_advice_storage: [_; 2];
-        let mut external_advice = None;
-
-        if let Some(jump_dest) = seg_def.spontaneous_jump_pc() {
-            let pc = prev_state.pc;
-            let cycle = b.cast(prev_state.cycle);
-            prev_state.pc = b.lit(jump_dest);
-            external_advice_storage = [pc, cycle];
-            external_advice = Some(&external_advice_storage as &[_]);
-        }
-
-        let (mut seg, kmem) = segment_builder.run(
-            idx, seg_def, prev_state, prev_kmem, external_advice,
-            move |w| {
-                let ew = project_witness(w);
-                &ew.trace.as_instr().segments[idx]
-            });
-        self.t.seg_graph_builder.set_final(idx, seg.final_state().clone());
-        self.t.seg_graph_builder.set_final_mem(idx, kmem);
-
-        // If this segment is actually used in the trace, find the relevant trace chunk and use its
-        // data to initialize the segment's secrets.
-        if let Some(&(chunk_idx, cycle)) = self.t.seg_user_map.get(&idx) {
-            let chunk = &exec.trace.as_instr().chunks[chunk_idx];
-
-            if self.c.check_steps > 0 {
-                seg.check_states(&self.c.cx, b, cycle, self.c.check_steps, &chunk.states);
-            }
-
-            // FIXME: this leaks information, namely, the identity of the last used segment.  We
-            // should either forbid mixing `--expect-zero` with public PC, or otherwise ensure that
-            // this is only used for testing.
-            if chunk_idx == exec.trace.as_instr().chunks.len() - 1 {
-                check_last(&self.c.cx, b, seg.final_state(), self.c.expect_zero);
-            }
-        }
+        self.c.init(b, exec, &seg_values);
+        self.t.init(&mut self.c, b, exec, &seg_values);
     }
 
     fn finish(
@@ -336,6 +177,63 @@ impl<'a> ExecBuilder<'a, InstrTraceBuilder<'a>> {
 }
 
 impl<'a> Common<'a> {
+    fn init(
+        &mut self,
+        b: &impl Builder<'a>,
+        exec: &ExecBody,
+        seg_values: &[Vec<TWire<'a, u64>>],
+    ) {
+        // Add extra `MemPort`s to enforce `expect_write`.
+        if let Some(addr) = self.expect_write {
+            // We write a 0 before execution begins, and try to read back a 1 after the program
+            // terminates.  This succeeds only if the program overwrites the 0 with a 1 during its
+            // execution.  We can't simply leave the memory uninitialized because reads from
+            // uninitialized memory are allowed (and the value produced is unconstrained).
+            self.mem.add_initial_write(b, addr, 0);
+            self.mem.add_final_read(b, addr, 1);
+        }
+
+        // Add hash check for the `commitment`.
+        if let Some(commitment) = exec.params.commitment {
+            let _g = b.scoped_label("check commitment");
+            match commitment {
+                Commitment::Sha256(expect_hash) => {
+                    let mut h = Sha256::new(b);
+
+                    for (cs, instrs) in exec.program.iter().zip(self.fetch.all_instrs().iter()) {
+                        if !cs.secret || cs.uncommitted {
+                            continue;
+                        }
+                        for instr in instrs {
+                            let RamInstrRepr { opcode, dest, op1, op2, imm } = instr.repr;
+                            h.push(b, opcode);
+                            h.push(b, dest);
+                            h.push(b, op1);
+                            h.push(b, op2);
+                            h.push(b, imm);
+                        }
+                    }
+
+                    for (seg, values) in exec.init_mem.iter().zip(seg_values.iter()) {
+                        if !seg.secret || seg.uncommitted {
+                            continue;
+                        }
+                        for &w in values {
+                            h.push(b, w);
+                        }
+                    }
+
+                    let actual_hash = h.finish(b);
+                    wire_assert!(
+                        cx = &self.cx, b, b.eq(actual_hash, b.lit(expect_hash)),
+                        "bad commitment: actual hash is {:?}, but expected {:?}",
+                        cx.eval(actual_hash), expect_hash,
+                    );
+                },
+            }
+        }
+    }
+
     fn finish(
         c: Rooted<'a, Self>,
         mh: &mut MigrateHandle<'a>,
@@ -364,6 +262,135 @@ impl<'a> Common<'a> {
 }
 
 impl<'a> InstrTraceBuilder<'a> {
+    fn init(
+        &mut self,
+        c: &mut Common<'a>,
+        b: &impl Builder<'a>,
+        exec: &ExecBody,
+        seg_values: &[Vec<TWire<'a, u64>>],
+    ) {
+        if let Some(ref out_path) = self.debug_segment_graph_path {
+            std::fs::write(out_path, self.seg_graph_builder.dump()).unwrap();
+        }
+
+        // Set up initial KnownMem
+        let mut kmem = KnownMem::with_default(b.lit(0));
+        for (seg, values) in exec.init_mem.iter().zip(seg_values.iter()) {
+            kmem.init_segment(seg, &values);
+        }
+        self.seg_graph_builder.set_cpu_init_mem(kmem);
+        debug_assert_eq!(seg_values.len(), exec.init_mem.len());
+
+        // Populate `seg_user_map`.
+        let mut cycle = 0;
+        for (i, chunk) in exec.trace.as_instr().chunks.iter().enumerate() {
+            if let Some(c) = chunk.debug.as_ref().and_then(|d| d.cycle) {
+                cycle = c;
+            }
+
+            let old = self.seg_user_map.insert(chunk.segment, (i, cycle));
+            assert!(old.is_none());
+
+            cycle += chunk.states.len() as u32;
+        }
+    }
+
+    fn run(
+        eb: &mut Rooted<'a, ExecBuilder<'a, Self>>,
+        mh: &mut MigrateHandle<'a>,
+        b: &impl Builder<'a>,
+        exec: &ExecBody,
+        project_witness: impl Fn(&MultiExecWitness) -> &ExecWitness + Copy + 'static,
+    ) {
+        let instr_lookup = InstrLookup::new(&exec.program);
+        for item in eb.open(mh).t.seg_graph_builder.get_order() {
+            match item {
+                SegGraphItem::Segment(idx) => {
+                    let mut eb = eb.open(mh);
+                    let eb = &mut *eb;
+                    eb.t.add_segment(&mut eb.c, b, exec, &instr_lookup, idx, project_witness);
+                },
+                SegGraphItem::Network => {
+                    unsafe { mh.erase_and_migrate(b.circuit()) };
+                    info!("seg_graph_builder.build_network");
+                    let mut seg_graph_builder = eb.project(mh, |eb| &mut eb.t.seg_graph_builder);
+                    SegGraphBuilder::build_network(&mut seg_graph_builder, mh, b, project_witness);
+                    unsafe { mh.erase_and_migrate(b.circuit()) };
+                    continue;
+                },
+            }
+
+            unsafe { mh.erase_and_migrate(b.circuit()) };
+        }
+    }
+
+    fn add_segment(
+        &mut self,
+        c: &mut Common<'a>,
+        b: &impl Builder<'a>,
+        exec: &ExecBody,
+        instr_lookup: &InstrLookup,
+        idx: usize,
+        project_witness: impl Fn(&MultiExecWitness) -> &ExecWitness + Copy + 'static,
+    ) {
+        // Build the circuit for this segment.
+        let mut segment_builder = SegmentBuilder {
+            cx: &c.cx,
+            b: b,
+            ev: &mut c.ev,
+            privilege_levels: c.privilege_levels,
+            calc_step_func: self.calc_step_func,
+            calc_step_inner_cases: &self.calc_step_inner_cases,
+            check_step_func: self.check_step_func,
+            mem: &mut c.mem,
+            fetch: &mut c.fetch,
+            params: &exec.params,
+            prog: instr_lookup,
+            check_steps: c.check_steps,
+        };
+
+        let seg_def = &exec.trace.as_instr().segments[idx];
+        let mut prev_state = self.seg_graph_builder.get_initial(b, idx).clone();
+        let prev_kmem = self.seg_graph_builder.take_initial_mem(idx);
+
+        let external_advice_storage: [_; 2];
+        let mut external_advice = None;
+
+        if let Some(jump_dest) = seg_def.spontaneous_jump_pc() {
+            let pc = prev_state.pc;
+            let cycle = b.cast(prev_state.cycle);
+            prev_state.pc = b.lit(jump_dest);
+            external_advice_storage = [pc, cycle];
+            external_advice = Some(&external_advice_storage as &[_]);
+        }
+
+        let (mut seg, kmem) = segment_builder.run(
+            idx, seg_def, prev_state, prev_kmem, external_advice,
+            move |w| {
+                let ew = project_witness(w);
+                &ew.trace.as_instr().segments[idx]
+            });
+        self.seg_graph_builder.set_final(idx, seg.final_state().clone());
+        self.seg_graph_builder.set_final_mem(idx, kmem);
+
+        // If this segment is actually used in the trace, find the relevant trace chunk and use its
+        // data to initialize the segment's secrets.
+        if let Some(&(chunk_idx, cycle)) = self.seg_user_map.get(&idx) {
+            let chunk = &exec.trace.as_instr().chunks[chunk_idx];
+
+            if c.check_steps > 0 {
+                seg.check_states(&c.cx, b, cycle, c.check_steps, &chunk.states);
+            }
+
+            // FIXME: this leaks information, namely, the identity of the last used segment.  We
+            // should either forbid mixing `--expect-zero` with public PC, or otherwise ensure that
+            // this is only used for testing.
+            if chunk_idx == exec.trace.as_instr().chunks.len() - 1 {
+                check_last(&c.cx, b, seg.final_state(), c.expect_zero);
+            }
+        }
+    }
+
     fn finish(
         t: Rooted<'a, Self>,
         mh: &mut MigrateHandle<'a>,
